5a844bb9fc36d527a2901786b6502400575943f6
Neil Mathew <mathew.neil@gmail.com>
2016-09-12 10:06:49 +0530

    Separate States from List Base Fragments

---
+++ app/src/main/java/com/kayako/sdk/android/k5/activities/KayakoArticleActivity.java
@@ -0,0 +1,15 @@
+package com.kayako.sdk.android.k5.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kayako.sdk.android.k5.R;
+
+public class KayakoArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ko__activity_article);
+    }
+}
