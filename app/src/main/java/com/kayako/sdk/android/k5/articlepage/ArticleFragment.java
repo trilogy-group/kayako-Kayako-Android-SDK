8bbba44d1362053ee1d9a91da0201435717436e8
Neil Mathew <mathew.neil@gmail.com>
2016-09-12 12:53:54 +0530

    Finish the UI of Article Page

--- app/src/main/java/com/kayako/sdk/android/k5/articlepage/ArticleFragment.java
+++ app/src/main/java/com/kayako/sdk/android/k5/articlepage/ArticleFragment.java
@@ -5,10 +5,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.kayako.sdk.android.k5.R;
 import com.kayako.sdk.android.k5.common.fragments.BaseStateFragment;
+import com.kayako.sdk.android.k5.common.view.CropCircleTransformation;
+import com.kayako.sdk.android.k5.common.view.RoundedCornersTransformation;
 import com.kayako.sdk.helpcenter.articles.Article;
+import com.kayako.sdk.helpcenter.category.Category;
+import com.kayako.sdk.helpcenter.section.Section;
 
 /**
  * @author Neil Mathew <neil.mathew@kayako.com>
@@ -17,6 +26,12 @@ public class ArticleFragment extends BaseStateFragment {
 
     public static final String ARG_ARTICLE = "article";
     private View mRoot;
+
+    private TextView articleTitle;
+    private TextView articleDirectoryPath;
+    private WebView articleContent;
+    private ImageView authorAvatar;
+    private TextView authorName;
 
     public static ArticleFragment newInstance(Article article) {
         Bundle bundle = new Bundle();
@@ -29,8 +44,37 @@ public class ArticleFragment extends BaseStateFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRoot = inflater.inflate(R.layout.ko__fragment_article_content, null);
-        initStateViews(mRoot);
+        super.initStateViews(mRoot);
         return mRoot;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Article article = (Article) getArguments().getSerializable(ARG_ARTICLE);
+
+        articleTitle = (TextView) mRoot.findViewById(R.id.ko__article_title);
+        articleTitle.setText(article.getTitle());
+
+        Section section = article.getSection();
+        Category category = section.getCategory();
+        articleDirectoryPath = (TextView) mRoot.findViewById(R.id.ko__article_directory);
+        articleDirectoryPath.setText(category.getTitle() + " > " + section.getTitle());
+
+        articleContent = (WebView) mRoot.findViewById(R.id.ko__article_webview);
+        articleContent.loadData(article.getContents(), "text/html; charset=utf-8", "UTF-8");
+        articleContent.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+        authorName = (TextView) mRoot.findViewById(R.id.ko__article_author_name);
+        authorName.setText(article.getAuthor().getFullName());
+
+        // Show Image
+        authorAvatar = (ImageView) mRoot.findViewById(R.id.ko__article_author_avatar); // TODO: Glide
+        Glide.with(getContext()).load(article.getAuthor().getAvatarUrl())
+                .placeholder(R.color.ko__dark_gray_image_background) // TODO: Repalce wi
+                .bitmapTransform(new CropCircleTransformation(getContext()))
+                .into(authorAvatar);
+
+    }
 }
