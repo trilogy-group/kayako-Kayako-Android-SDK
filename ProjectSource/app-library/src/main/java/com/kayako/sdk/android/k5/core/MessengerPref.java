74fe2ded26c05275fd3535831da80b1ef7240366
Neil Mathew <mathew.neil@gmail.com>
2017-03-02 16:35:42 +0530

    Add fullName, Email and userId to store

--- ProjectSource/app-library/src/main/java/com/kayako/sdk/android/k5/core/MessengerPref.java
+++ ProjectSource/app-library/src/main/java/com/kayako/sdk/android/k5/core/MessengerPref.java
@@ -7,6 +7,10 @@ public class MessengerPref {
     final private String PREF_NAME = "kayako_messenger_info";
     final private String KEY_FINGERPRINT_ID = "fingerprint_id";
 
+    final private String KEY_EMAIL_ID = "email_id";
+    final private String KEY_FULL_NAME = "full_name";
+    final private String KEY_CURRENT_USER_ID = "current_user_id";
+
     private static MessengerPref sInstance;
     private static SharedPreferences sPrefs;
     private Context mContext;
@@ -37,4 +41,32 @@ public class MessengerPref {
         sPrefs.edit().putString(KEY_FINGERPRINT_ID, fingerprintId).apply();
     }
 
+    public String getEmailId() {
+        return sPrefs.getString(KEY_EMAIL_ID, null);
+    }
+
+    public void setEmailId(String email) {
+        sPrefs.edit().putString(KEY_EMAIL_ID, email).commit();
+    }
+
+    public String getFullName() {
+        return sPrefs.getString(KEY_FULL_NAME, null);
+    }
+
+    public void setFullName(String fullName) {
+        sPrefs.edit().putString(KEY_FULL_NAME, fullName).commit();
+    }
+
+    public Long getUserId() {
+        long userId = sPrefs.getLong(KEY_CURRENT_USER_ID, 0);
+        if (userId == 0) {
+            return null;
+        } else {
+            return userId;
+        }
+    }
+
+    public void setUserId(long userId) {
+        sPrefs.edit().putLong(KEY_CURRENT_USER_ID, userId).commit();
+    }
 }
