8f1092e62a7af0088ec431b2f6d98e4b36bbc569
Neil Mathew <mathew.neil@gmail.com>
2017-05-28 10:36:08 +0530

    Scroll to new messages if nearby

--- ProjectSource/app-library/src/main/java/com/kayako/sdk/android/k5/messenger/messagelistpage/MessageListFragment.java
+++ ProjectSource/app-library/src/main/java/com/kayako/sdk/android/k5/messenger/messagelistpage/MessageListFragment.java
@@ -95,6 +95,9 @@ public class MessageListFragment extends MessengerListFragment implements Messag
             if (mLoadMoreListener != null) {
                 super.setLoadMoreListener(mLoadMoreListener);
             }
+
+            super.scrollToNewMessagesIfNearby();
+
             mIsListAlreadyInitialized = true;
         }
 
