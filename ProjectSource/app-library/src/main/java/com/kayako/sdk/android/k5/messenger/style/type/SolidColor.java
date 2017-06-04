89dd38bc2e497223d4092a650700146605667ad6
Neil Mathew <mathew.neil@gmail.com>
2017-06-05 00:53:35 +0530

    Add constructor for easy conversion of ColorResId to HexColorString

--- ProjectSource/app-library/src/main/java/com/kayako/sdk/android/k5/messenger/style/type/SolidColor.java
+++ ProjectSource/app-library/src/main/java/com/kayako/sdk/android/k5/messenger/style/type/SolidColor.java
@@ -3,6 +3,10 @@ package com.kayako.sdk.android.k5.messenger.style.type;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
+
+import com.kayako.sdk.android.k5.core.Kayako;
 
 public class SolidColor extends Background {
 
@@ -18,6 +22,22 @@ public class SolidColor extends Background {
         if (hexColor == null) {
             throw new IllegalArgumentException("HexColor can not be null");
         }
+
+        // Conversion is done in the constructor so that if it fails, the developer is informed at the time of creation
+        Color.parseColor(hexColor);
+    }
+
+    public SolidColor(@ColorRes int colorResId, boolean isDarkColor) {
+        super(BackgroundType.SOLID_COLOR);
+        this.hexColor = String.format("#%X", ContextCompat.getColor(Kayako.getApplicationContext(), colorResId));
+        this.isDarkColor = isDarkColor;
+
+        if (hexColor == null) {
+            throw new IllegalArgumentException("HexColor can not be null");
+        }
+
+        // Conversion is done in the constructor so that if it fails, the developer is informed at the time of creation
+        Color.parseColor(hexColor);
     }
 
     @Override
