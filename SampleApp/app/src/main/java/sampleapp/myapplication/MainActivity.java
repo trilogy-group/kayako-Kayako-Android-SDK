3fd5cb933c7345b97b0c6d1d185ee3b1aee5d29f
Neil Mathew <mathew.neil@gmail.com>
2017-06-09 20:23:31 +0530

    Update Sample app to use Messenger SDK

--- SampleApp/app/src/main/java/sampleapp/myapplication/MainActivity.java
+++ SampleApp/app/src/main/java/sampleapp/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package sampleapp.myapplication;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.kayako.sdk.android.k5.core.Kayako;
+import com.kayako.sdk.android.k5.messenger.style.ForegroundFactory;
+import com.kayako.sdk.android.k5.messenger.style.type.SolidColor;
 
 import java.util.Locale;
 
@@ -15,11 +18,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Context context = MainActivity.this;
-        java.util.Locale locale = Locale.ENGLISH;
-        String helpCenterUrl = "http://support.kayako.com";
+        final Context context = MainActivity.this;
+        final java.util.Locale locale = Locale.ENGLISH;
+        final String helpCenterUrl = "http://support.kayako.com";
 
-        // Open Help Center using the following line - Kayako
-        Kayako.getInstance().openHelpCenter(this, helpCenterUrl, locale);
+        findViewById(R.id.button_helpcenter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Help Center using the following line - Kayako
+                Kayako.getInstance().openHelpCenter(context, helpCenterUrl, locale);
+            }
+        });
+
+        findViewById(R.id.button_messenger).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Messenger using the following lines - Kayako
+                Kayako.getInstance().getMessenger()
+                        .setUrl("https://kayako-mobile-testing.kayako.com")
+                        .setBrandName("Brewfictus")
+                        .setTitle("Howdy")
+                        .setDescription("We make quality coffee at a price you can afford. How can we help?")
+                        .setBackground(new SolidColor(R.color.colorPrimary, true))
+                        .setForeground(ForegroundFactory.getForeground(ForegroundFactory.ForegroundOption.CONFETTI))
+                        .open(MainActivity.this);
+            }
+        });
+
     }
 }
