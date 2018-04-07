d5b1d94cac108ffaf832f53ada515a33ff22f70b
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-07 21:02:55 +0530

    updated for AttachmentFileType

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/AttachmentFileTypeTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/AttachmentFileTypeTest.java
@@ -1,44 +1,53 @@
 package com.kayako.sdk.android.k5.common.adapter.messengerlist;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import java.io.File;
-import static org.junit.Assert.*;
 
 public class AttachmentFileTypeTest {
 
+    @Rule
+    public ErrorCollector errorCollector  = new ErrorCollector();
+
+    private static final String CAPTION = "file type test";
+
     @Test(expected = AssertionError.class)
     public void test_constructor1() {
-        AttachmentFileType attachmentFileType = new AttachmentFileType(1L, null, "file type test");
+        AttachmentFileType attachmentFileType = new AttachmentFileType(1L, null, CAPTION);
     }
 
     @Test
     public void test_constructor2() {
         File file = new File("");
-        AttachmentFileType attachmentFileType = new AttachmentFileType(1L, file, "file type test");
-        assertNotNull(attachmentFileType);
-        assertEquals(AttachmentFileType.TYPE.FILE, attachmentFileType.getType());
+        AttachmentFileType attachmentFileType = new AttachmentFileType(1L, file, CAPTION);
+        errorCollector.checkThat(attachmentFileType, notNullValue());
+        errorCollector.checkThat(AttachmentFileType.TYPE.FILE, equalTo(attachmentFileType.getType()));
     }
 
     @Test
     public void test_getId() {
         AttachmentFileType attachmentFileType = getAttachmentFileType();
-        assertEquals(1L, attachmentFileType.getId().longValue());
+        errorCollector.checkThat(1L, is(attachmentFileType.getId().longValue()));
     }
 
     @Test
     public void test_getCaption() {
         AttachmentFileType attachmentFileType = getAttachmentFileType();
-        assertEquals("file type test", attachmentFileType.getCaption());
+        errorCollector.checkThat(CAPTION, equalTo(attachmentFileType.getCaption()));
     }
 
     @Test
     public void test_getThumbnailFile() {
         AttachmentFileType attachmentFileType = getAttachmentFileType();
-        assertNotNull(attachmentFileType.getThumbnailFile());
+        errorCollector.checkThat(attachmentFileType.getThumbnailFile(), notNullValue());
     }
 
     private AttachmentFileType getAttachmentFileType() {
         File file = new File("");
-        return new AttachmentFileType(1L, file, "file type test");
+        return new AttachmentFileType(1L, file, CAPTION);
     }
 }
