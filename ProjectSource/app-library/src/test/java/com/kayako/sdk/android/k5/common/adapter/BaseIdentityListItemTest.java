721043a7cbcbef50cf6cb83244ae0704bacef468
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-07 19:39:17 +0400

    Unit test for BaseentityListItem class

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/BaseIdentityListItemTest.java
@@ -0,0 +1,35 @@
+package com.kayako.sdk.android.k5.common.adapter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BaseIdentityListItemTest {
+
+    private static final Long ID = 0L;
+
+    @Mock
+    BaseIdentityListItem baseIdentityListItem;
+
+
+    @Before
+    public void setUp() throws Exception {
+        baseIdentityListItem.setId(ID);
+    }
+
+    @Test
+    public void getIdTest() {
+        assertEquals(baseIdentityListItem.getId(), ID);
+    }
+
+    @Test
+    public void setIdTest() {
+        assertEquals(baseIdentityListItem.getId(), ID);
+    }
+
+}
