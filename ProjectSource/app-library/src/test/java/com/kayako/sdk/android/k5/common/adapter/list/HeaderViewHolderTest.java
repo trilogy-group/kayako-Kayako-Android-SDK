0e2322338c09b3a46f38c56b14a4a3d898417501
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-07 13:11:40 +0400

    Unit test for ListItemViewHolder class, added review suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
@@ -6,14 +6,19 @@ import android.widget.TextView;
 import com.kayako.sdk.android.k5.R;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.when;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
@@ -24,6 +29,8 @@ import static org.junit.Assert.*;
 @RunWith(MockitoJUnitRunner.class)
 public class HeaderViewHolderTest {
 
+    @Rule
+    public final ErrorCollector collector = new ErrorCollector();
     @Mock
     View view;
 
@@ -42,9 +49,9 @@ public class HeaderViewHolderTest {
     public void constructorMViewTest() {
         verify(view, times(1)).findViewById(anyInt());
 
-        assertThat(headerViewHolder.mView, notNullValue());
-        assertThat(headerViewHolder.mView, is(instanceOf(View.class)));
-        assertThat(headerViewHolder.mView, is(equalTo(view)));
+        collector.checkThat(headerViewHolder.mView, notNullValue());
+        collector.checkThat(headerViewHolder.mView, is(instanceOf(View.class)));
+        collector.checkThat(headerViewHolder.mView, is(equalTo(view)));
     }
 
     @Test
@@ -52,8 +59,8 @@ public class HeaderViewHolderTest {
         TextView textViewMTitle = (TextView) view.findViewById(R.id.ko__list_header_title);
         verify(view, times(2)).findViewById(anyInt());
 
-        assertThat(headerViewHolder.mTitle, notNullValue());
-        assertThat(headerViewHolder.mTitle, is(instanceOf(TextView.class)));
-        assertThat(headerViewHolder.mTitle, is(equalTo(textViewMTitle)));
+        collector.checkThat(headerViewHolder.mTitle, notNullValue());
+        collector.checkThat(headerViewHolder.mTitle, is(instanceOf(TextView.class)));
+        collector.checkThat(headerViewHolder.mTitle, is(equalTo(textViewMTitle)));
     }
 }
