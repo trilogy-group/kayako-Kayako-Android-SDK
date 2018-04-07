0b0a64f65a4642799e176e3a656acc2bb0a8c208
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-07 23:41:49 +0530

    updated unit test case for UserDecoration

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/UserDecorationTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/UserDecorationTest.java
@@ -1,101 +1,105 @@
 package com.kayako.sdk.android.k5.common.adapter.messengerlist;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
+import org.junit.rules.ErrorCollector;
 import java.util.Map;
-
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
 
 public class UserDecorationTest {
 
-    private String name;
-    private Long userId;
-    private String avatarUrl;
+    private static final String NAME = "name";
+    private static final String AVATARURL = "/avatarUrl";
     private boolean isSelf;
+    private Long userId;
     private UserDecoration userDecoration;
+
+    @Rule
+    public ErrorCollector errorCollector = new ErrorCollector();
 
     @Before
     public void setup() {
-        name = "name";
         userId = 1L;
-        avatarUrl = "/avatarUrl";
         isSelf = true;
-
-        userDecoration = new UserDecoration(name, avatarUrl, userId, isSelf);
+        userDecoration = new UserDecoration(NAME, AVATARURL, userId, isSelf);
     }
 
     @Test
     public void test_constructor1() {
-        assertNotNull(userDecoration);
-        assertEquals(name, userDecoration.getName());
-        assertEquals(avatarUrl, userDecoration.getAvatarUrl());
-        assertNotEquals(2L, userDecoration.getUserId().longValue());
+        errorCollector.checkThat(userDecoration, notNullValue());
+        errorCollector.checkThat(userDecoration.getName(), is(equalTo(NAME)));
+        errorCollector.checkThat(userDecoration.getAvatarUrl(), is(equalTo(AVATARURL)));
+        errorCollector.checkThat(userDecoration.getUserId().longValue(), is(not(2L)));
     }
 
     @Test
     public void test_constructor2() {
-        UserDecoration userDecoration1 = new UserDecoration(avatarUrl, userId, isSelf);
-        assertNotNull(userDecoration1);
-        assertEquals(avatarUrl, userDecoration1.getAvatarUrl());
-        assertNull(userDecoration1.getName());
-        assertTrue(userDecoration1.isSelf());
+        UserDecoration userDecoration1 = new UserDecoration(AVATARURL, userId, isSelf);
+        errorCollector.checkThat(userDecoration1, notNullValue());
+        errorCollector.checkThat(userDecoration1.getAvatarUrl(), is(equalTo(AVATARURL)));
+        errorCollector.checkThat(userDecoration1.getName(), nullValue());
+        errorCollector.checkThat(userDecoration1.isSelf(), is(true));
     }
 
     @Test
     public void test_getName() {
-        assertEquals(name, userDecoration.getName());
+        errorCollector.checkThat(userDecoration.getName(), is(equalTo(NAME)));
     }
 
     @Test
     public void test_setName() {
         userDecoration.setName("name_2");
-        assertNotEquals(name, userDecoration.getName());
-        assertEquals("name_2", userDecoration.getName());
+        errorCollector.checkThat(userDecoration.getName(), is(not(NAME)));
+        errorCollector.checkThat(userDecoration.getName(), is(equalTo("name_2")));
     }
 
     @Test
     public void test_getUserId() {
-        assertEquals(userId, userDecoration.getUserId());
+        errorCollector.checkThat(userDecoration.getUserId(), is(userId));
     }
 
     @Test
     public void test_setUserId() {
         userDecoration.setUserId(0L);
-        assertNotEquals(userId, userDecoration.getUserId());
-        assertEquals(0L, userDecoration.getUserId().longValue());
+        errorCollector.checkThat(userDecoration.getUserId(), is(not(userId)));
+        errorCollector.checkThat(userDecoration.getUserId().longValue(), is(0L));
     }
 
     @Test
     public void test_getAvatarUrl() {
-        assertEquals(avatarUrl, userDecoration.getAvatarUrl());
+        errorCollector.checkThat(userDecoration.getAvatarUrl(), is(equalTo(AVATARURL)));
     }
 
     @Test
     public void test_setAvatarUrl() {
         userDecoration.setAvatarUrl("/avatarUrl2");
-        assertNotEquals(avatarUrl, userDecoration.getAvatarUrl());
-        assertEquals("/avatarUrl2", userDecoration.getAvatarUrl());
+        errorCollector.checkThat(userDecoration.getAvatarUrl(), is(not(AVATARURL)));
+        errorCollector.checkThat(userDecoration.getAvatarUrl(), is(equalTo("/avatarUrl2")));
     }
 
     @Test
     public void test_isSelf() {
-        assertTrue(userDecoration.isSelf());
+        errorCollector.checkThat(userDecoration.isSelf(), is(true));
     }
 
     @Test
     public void test_setSelf() {
         userDecoration.setSelf(false);
-        assertFalse(userDecoration.isSelf());
+        errorCollector.checkThat(userDecoration.isSelf(), is(false));
     }
 
     @Test
     public void test_getContents() {
         Map map = userDecoration.getContents();
-        assertEquals(4, map.size());
-        assertEquals(name, map.get("name"));
-        assertEquals(userId.toString(), map.get("userId"));
-        assertEquals(avatarUrl, map.get("avatarUrl"));
-        assertEquals("true", map.get("isSelf"));
+        errorCollector.checkThat(map.size(), is(4));
+        errorCollector.checkThat(map.get("name").toString(), is(equalTo(NAME)));
+        errorCollector.checkThat(map.get("userId").toString(), is(equalTo(userId.toString())));
+        errorCollector.checkThat(map.get("avatarUrl").toString(), is(equalTo(AVATARURL)));
+        errorCollector.checkThat(map.get("isSelf").toString(), is(equalTo("true")));
     }
 }
