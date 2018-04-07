f595a2d231d8972bdba5239a467483a6fba36a14
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-08 00:52:25 +0400

    Unit test for SearchSectionListType class

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/searchsectionlist/SearchSectionListTypeTest.java
@@ -0,0 +1,19 @@
+package com.kayako.sdk.android.k5.common.adapter.searchsectionlist;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SearchSectionListTypeTest {
+
+    private static final int SEARCH_SECTION_ITEM = 4;
+
+    @Test
+    public void testListItemValueIsCorrect() {
+        assertThat(SearchSectionListType.SEARCH_SECTION_ITEM, is(equalTo(SEARCH_SECTION_ITEM)));
+    }
+
+}
