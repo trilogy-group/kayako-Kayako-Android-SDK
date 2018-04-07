bccbe34ae0a623838773a6bac39bd06886401f6b
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-08 00:35:53 +0400

    Unit test for ListType class

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/ListTypeTest.java
@@ -0,0 +1,24 @@
+package com.kayako.sdk.android.k5.common.adapter.list;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class ListTypeTest {
+
+    private static final int LIST_ITEM = 2;
+    private static final int HEADER_ITEM = 3;
+
+    @Test
+    public void testListItemValueIsCorrect() {
+        assertThat(ListType.LIST_ITEM, is(equalTo(LIST_ITEM)));
+    }
+
+    @Test
+    public void testHeaderItemValueIsCorrect() {
+        assertThat(ListType.HEADER_ITEM, is(equalTo(HEADER_ITEM)));
+    }
+}
