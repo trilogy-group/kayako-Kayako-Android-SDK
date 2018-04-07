a15e95763a19759b7f290888bd3a46be8c8902e5
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-08 00:32:24 +0400

    Unit test for ConversationListType class

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/conversationlist/ConversationListTypeTest.java
@@ -0,0 +1,20 @@
+package com.kayako.sdk.android.k5.common.adapter.conversationlist;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConversationListTypeTest {
+
+    private static final int CONVERSATION_LIST_ITEM = 2;
+
+    @Test
+    public void testListItemValueIsCorrect() {
+        assertThat(ConversationListType.CONVERSATION_LIST_ITEM, is(equalTo(2)));
+    }
+
+
+}
