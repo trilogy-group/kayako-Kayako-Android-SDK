9d51bc9b409e9f6ebe43e551af44ed7d411bad47
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-07 18:52:56 +0400

    Unit test for SpinnerItem class

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/spinnerlist/SpinnerItemTest.java
@@ -0,0 +1,56 @@
+package com.kayako.sdk.android.k5.common.adapter.spinnerlist;
+
+import com.kayako.sdk.base.parser.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SpinnerItemTest {
+
+    @Mock
+    private Resource resource;
+
+    private static final SpinnerItem spinnerItem = new SpinnerItem();
+
+    private static final String LABEL = "labelTest";
+
+    @Test
+    public void getResourceTest() {
+        spinnerItem.setResource(resource);
+        assertThat(spinnerItem.getResource(), is(equalTo(resource)));
+    }
+
+    @Test
+    public void setResourceTest() {
+        spinnerItem.setResource(resource);
+        assertThat(spinnerItem.getResource(), is(equalTo(resource)));
+    }
+
+    @Test
+    public void getLabel() {
+        spinnerItem.setLabel(LABEL);
+        assertThat(spinnerItem.getLabel(), is(equalTo(LABEL)));
+    }
+
+    @Test
+    public void setLabel() {
+        spinnerItem.setLabel(LABEL);
+        assertThat(spinnerItem.getLabel(), is(equalTo(LABEL)));
+    }
+
+    @Test
+    public void equalsTest() {
+        SpinnerItem spinnerItem1 = new SpinnerItem();
+        spinnerItem.setResource(resource);
+        spinnerItem1.setResource(resource);
+        assertEquals(true, spinnerItem.equals(spinnerItem1));
+    }
+}
