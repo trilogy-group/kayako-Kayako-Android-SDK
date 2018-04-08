06f3346b63620054c4fb771299c2d358d976cc99
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-08 23:30:24 +0530

    Updated unit test for BotMessageListItem

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/BotMessageListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/BotMessageListItemTest.java
@@ -15,7 +15,7 @@ public class BotMessageListItemTest {
     private BotMessageListItem botMessageListItem;
 
     @Rule
-    public ErrorCollector errorCollector = new ErrorCollector();
+    public final ErrorCollector errorCollector = new ErrorCollector();
 
     @Before
     public void setUp() {
@@ -24,35 +24,35 @@ public class BotMessageListItemTest {
     }
 
     @Test
-    public void test_constructor() {
+    public void whenValidParamsConstructorThenObjectCreated() {
         errorCollector.checkThat(botMessageListItem, notNullValue());
         errorCollector.checkThat(botMessageListItem.getTime(), is(time));
     }
 
     @Test
-    public void test_getMessage(){
+    public void getMessage(){
         errorCollector.checkThat(botMessageListItem.getMessage(), is(equalTo(message)));
     }
 
     @Test
-    public void test_setMessage(){
+    public void setMessage(){
         botMessageListItem.setMessage("test");
         errorCollector.checkThat(botMessageListItem.getMessage(), is(equalTo("test")));
     }
 
     @Test
-    public void test_getTime(){
+    public void getTime(){
         errorCollector.checkThat(botMessageListItem.getTime(), is(time));
     }
 
     @Test
-    public void test_setTime(){
+    public void setTime(){
         botMessageListItem.setTime(2000L);
         errorCollector.checkThat(botMessageListItem.getTime(), is(2000L));
     }
 
     @Test
-    public void test_getContents() {
+    public void getContents() {
         errorCollector.checkThat(botMessageListItem.getContents().size() > 0, is(true));
         errorCollector.checkThat(botMessageListItem.getContents().size(), is(2));
         errorCollector.checkThat(botMessageListItem.getContents().get("message"), is(equalTo(message)));
