444a401a3b79bf3c04031a59b5cc62c47d5274e1
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-08 11:07:55 +0530

    Unit test case for AttachmentMessageOtherListItem

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/AttachmentMessageOtherListItemTest.java
@@ -0,0 +1,102 @@
+package com.kayako.sdk.android.k5.common.adapter.messengerlist.view;
+
+import com.kayako.sdk.android.k5.common.adapter.messengerlist.Attachment;
+import com.kayako.sdk.android.k5.common.adapter.messengerlist.ChannelDecoration;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AttachmentMessageOtherListItemTest {
+
+    private static final String AVATARURL = "/avatarUrl";
+    private long time;
+    private ChannelDecoration channel;
+    private Attachment attachment;
+    private Long id;
+    private Map<String, Object> data;
+    private AttachmentMessageOtherListItem item;
+
+    @Rule
+    public ErrorCollector errorCollector = new ErrorCollector();
+
+    @Before
+    public void setUp() {
+        id = 1L;
+        data = new HashMap<String, Object>();
+        time = 1000L;
+        attachment = new Attachment(Attachment.TYPE.URL);
+        channel = new ChannelDecoration(1);
+        item = new AttachmentMessageOtherListItem(id, AVATARURL, channel, attachment, time, data);
+    }
+
+    @Test
+    public void test_constructor() {
+        errorCollector.checkThat(item, notNullValue());
+        errorCollector.checkThat(item.getTime(), is(time));
+    }
+
+    @Test
+    public void test_getAttachment() {
+        errorCollector.checkThat(item.getAttachment(), is(attachment));
+        errorCollector.checkThat(item.getAttachment().getType(), is(Attachment.TYPE.URL));
+
+    }
+
+    @Test
+    public void test_setAttachment() {
+        Attachment attachment1 = new Attachment(Attachment.TYPE.FILE);
+        item.setAttachment(attachment1);
+        errorCollector.checkThat(item.getAttachment(), is(equalTo(attachment1)));
+        errorCollector.checkThat(item.getAttachment().getType(), is(Attachment.TYPE.FILE));
+    }
+
+    @Test
+    public void test_getTime(){
+        errorCollector.checkThat(item.getTime(), is(time));
+    }
+
+    @Test
+    public void test_setTime(){
+        item.setTime(2000L);
+        errorCollector.checkThat(item.getTime(), is(2000L));
+    }
+
+    @Test
+    public void test_getAvatarUrl(){
+        errorCollector.checkThat(item.getAvatarUrl(), is(AVATARURL));
+    }
+
+    @Test
+    public void test_setAvatarUrl(){
+        item.setAvatarUrl("/avatarUrl2");
+        errorCollector.checkThat(item.getAvatarUrl(), is("/avatarUrl2"));
+    }
+
+    @Test
+    public void test_getChannel() {
+        errorCollector.checkThat(item.getChannel(), is(channel));
+    }
+
+    @Test
+    public void test_setChannel() {
+        ChannelDecoration channel1 = new ChannelDecoration(2);
+        item.setChannel(channel1);
+        errorCollector.checkThat(item.getChannel(), is(channel1));
+        errorCollector.checkThat(item.getChannel().getSourceDrawable(), is(2));
+    }
+
+    @Test
+    public void test_getContents() {
+        errorCollector.checkThat(item.getContents().size() > 0, is(true));
+        errorCollector.checkThat(item.getContents().size(), is(6));
+        errorCollector.checkThat(item.getContents().get("time"), is(equalTo(String.valueOf(time))));
+    }
+
+}
