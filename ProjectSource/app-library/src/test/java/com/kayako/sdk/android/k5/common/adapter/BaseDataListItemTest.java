807942ae4457d5cb3f82f95d23b6f5223de55d76
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-08 22:57:54 +0400

    [KAYAKO-743][Samir Aghayarov] applied suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/BaseDataListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/BaseDataListItemTest.java
@@ -1,13 +1,10 @@
 package com.kayako.sdk.android.k5.common.adapter;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -25,12 +22,6 @@ public class BaseDataListItemTest {
     @Test
     public void getData() {
         baseDataListItem.setData(data);
-        assertEquals(baseDataListItem.getData(), data);
-    }
-
-    @Test
-    public void setData() {
-        baseDataListItem.setData(data);
-        assertEquals(baseDataListItem.getData(), data);
+        assertEquals(data, baseDataListItem.getData());
     }
 }
