d1b81a25a3578e6d719046fa9a48cdd3b4135f5c
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-08 23:35:04 +0530

    Updated unit test for InputFeedbackCommentListItem

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/InputFeedbackCommentListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/InputFeedbackCommentListItemTest.java
@@ -19,7 +19,7 @@ public class InputFeedbackCommentListItemTest {
     private InputFeedbackCommentListItem listItem;
 
     @Rule
-    public ErrorCollector errorCollector = new ErrorCollector();
+    public final ErrorCollector errorCollector = new ErrorCollector();
 
     @Before
     public void setUp() {
@@ -29,52 +29,52 @@ public class InputFeedbackCommentListItemTest {
     }
 
     @Test
-    public void test_constructor(){
+    public void whenValidParamsConstructorThenObjectCreated(){
         errorCollector.checkThat(listItem, notNullValue());
         errorCollector.checkThat(listItem.getInstructionMessage(), is(equalTo(INSTRUCTIONMESSAGE)));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_constructor1(){
+    public void whenNullInstructionMessageThenException(){
         listItem = new InputFeedbackCommentListItem(null, rating, onAddFeedbackComment);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_constructor2(){
+    public void whenNullRatingThenIllegalArgumentException(){
         rating = null;
         listItem = new InputFeedbackCommentListItem(INSTRUCTIONMESSAGE, rating, onAddFeedbackComment);
     }
 
     @Test
-    public void test_constructor3(){
+    public void validParamsGivenInConstructorThenObjectCreated(){
         listItem = new InputFeedbackCommentListItem(INSTRUCTIONMESSAGE, "submittedComment");
         errorCollector.checkThat(listItem.getItemType(), is(equalTo(MessengerListType.INPUT_FIELD_FEEDBACK_COMMENT)));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void test_constructor4(){
+    public void whenNullSubmittedCommentThenException(){
         listItem = new InputFeedbackCommentListItem(INSTRUCTIONMESSAGE, null);
         errorCollector.checkThat(listItem.getItemType(), is(MessengerListType.INPUT_FIELD_FEEDBACK_COMMENT));
     }
 
     @Test
-    public void test_getInstructionMessage() {
+    public void getInstructionMessage() {
         errorCollector.checkThat(listItem.getInstructionMessage(), is(equalTo(INSTRUCTIONMESSAGE)));
     }
 
     @Test
-    public void test_getRating() {
+    public void getRating() {
         errorCollector.checkThat(listItem.getRating().name(), is(equalTo("GOOD")));
     }
 
     @Test
-    public void test_getOnAddFeedbackComment() {
+    public void getOnAddFeedbackComment() {
         errorCollector.checkThat(listItem.getOnAddFeedbackComment(), notNullValue());
         errorCollector.checkThat(listItem.getOnAddFeedbackComment(), is(onAddFeedbackComment));
     }
 
     @Test
-    public void test_getContents() {
+    public void getContents() {
         listItem = new InputFeedbackCommentListItem(INSTRUCTIONMESSAGE, "submittedComment");
         errorCollector.checkThat(listItem.getContents().size() > 0, is(true));
         errorCollector.checkThat(listItem.getContents().get("instructionMessage"), is(equalTo(INSTRUCTIONMESSAGE)));
