eef1bcc7af8f7fe470fb774016a12e364eaa6a27
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-10 02:31:35 +0400

    applied review suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.anyInt;
@@ -22,43 +23,41 @@ import static org.mockito.Mockito.when;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class HeaderViewHolderTest {
 
+    private HeaderViewHolder headerViewHolder;
+
     @Rule
     public final ErrorCollector collector = new ErrorCollector();
-    @Mock
-    View view;
 
     @Mock
-    TextView textView;
+    private View view;
 
-    HeaderViewHolder headerViewHolder;
+    @Mock
+    private TextView textView;
 
     @Before
     public void setUp() {
-        when(view.findViewById(anyInt())).thenReturn(textView);
+        when(view.findViewById(eq(R.id.ko__list_header_title))).thenReturn(textView);
         headerViewHolder = new HeaderViewHolder(view);
     }
 
     @Test
-    public void constructorMViewTest() {
-        verify(view, times(1)).findViewById(anyInt());
+    public void constructorMView() {
+        verify(view, times(1)).findViewById(eq(R.id.ko__list_header_title));
 
-        collector.checkThat(headerViewHolder.mView, notNullValue());
         collector.checkThat(headerViewHolder.mView, is(instanceOf(View.class)));
         collector.checkThat(headerViewHolder.mView, is(equalTo(view)));
     }
 
     @Test
-    public void constructorMTitleTest() {
+    public void constructorMTitle() {
         TextView textViewMTitle = (TextView) view.findViewById(R.id.ko__list_header_title);
         verify(view, times(2)).findViewById(anyInt());
 
-        collector.checkThat(headerViewHolder.mTitle, notNullValue());
         collector.checkThat(headerViewHolder.mTitle, is(instanceOf(TextView.class)));
         collector.checkThat(headerViewHolder.mTitle, is(equalTo(textViewMTitle)));
     }
