1657059f5fbc5f2f522262dee6956ca215a9a38f
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-09 19:25:39 +0400

    applied review suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/searchsectionlist/SearchSectionListTypeTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/searchsectionlist/SearchSectionListTypeTest.java
@@ -1,6 +1,5 @@
 package com.kayako.sdk.android.k5.common.adapter.searchsectionlist;
 
-
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,7 +11,7 @@ public class SearchSectionListTypeTest {
     private static final int SEARCH_SECTION_ITEM = 4;
 
     @Test
-    public void testListItemValueIsCorrect() {
+    public void listItemValueIsCorrect() {
         assertThat(SearchSectionListType.SEARCH_SECTION_ITEM, is(equalTo(SEARCH_SECTION_ITEM)));
     }
 
