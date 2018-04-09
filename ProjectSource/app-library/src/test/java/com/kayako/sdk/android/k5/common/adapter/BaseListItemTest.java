6efe12a786f58a47b551025d09d299ca8067c885
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-09 18:41:31 +0400

    applied review suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/BaseListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/BaseListItemTest.java
@@ -1,18 +1,13 @@
 package com.kayako.sdk.android.k5.common.adapter;
 
-
-import junit.framework.Assert;
-
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.Field;
-import java.util.Map;
 
+import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -21,19 +16,21 @@ public class BaseListItemTest {
 
     private static final int LIST_ITEM_TYPE = 4;
 
-
     @Mock
     BaseListItem baseListItem;
 
     @Test
     public void getData() throws Exception {
+        //Arrange
         // class is abstract and field is private , going to use reflection
         Field f1 = baseListItem.getClass().getSuperclass().getDeclaredField("listItemType");
         f1.setAccessible(true);
         f1.set(baseListItem, LIST_ITEM_TYPE);
+
+        // Act
         int expected = (Integer) f1.get(baseListItem);
-        MatcherAssert.assertThat(LIST_ITEM_TYPE, is(equalTo(expected)));
+
+        // Assert
+        assertThat(LIST_ITEM_TYPE, is(equalTo(expected)));
     }
-
-
 }
