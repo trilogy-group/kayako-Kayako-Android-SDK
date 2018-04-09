8b2f41455a36b2dc6ff922a8dc3e0d3f1fa1ac2a
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-10 01:31:48 +0400

    applied review suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/conversationlist/ConversationItemViewHolderTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/conversationlist/ConversationItemViewHolderTest.java
@@ -18,7 +18,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,19 +25,22 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class ConversationItemViewHolderTest {
 
+    private ConversationItemViewHolder conversationItemViewHolder;
+
     @Rule
-    public ErrorCollector collector = new ErrorCollector();
+    public final ErrorCollector collector = new ErrorCollector();
 
     @Mock
-    View view;
-    @Mock
-    TextView textView;
-    @Mock
-    ImageView imageView;
-    @Mock
-    RelativeLayout relativeLayout;
+    private View view;
 
-    ConversationItemViewHolder conversationItemViewHolder;
+    @Mock
+    private TextView textView;
+
+    @Mock
+    private ImageView imageView;
+
+    @Mock
+    private RelativeLayout relativeLayout;
 
     @Before
     public void setUp() {
@@ -55,91 +57,80 @@ public class ConversationItemViewHolderTest {
     }
 
     @Test
-    public void constructorItemViewTest() {
-        collector.checkThat(conversationItemViewHolder.itemView, notNullValue());
+    public void constructorItemView() {
         collector.checkThat(conversationItemViewHolder.itemView, is(instanceOf(View.class)));
         collector.checkThat(conversationItemViewHolder.itemView, is(equalTo(view)));
     }
 
     @Test
-    public void constructorAvatarViewTest() {
+    public void constructorAvatarView() {
         ImageView avatar = (ImageView) view.findViewById(R.id.ko__avatar);
 
         verify(view, times(2)).findViewById(R.id.ko__avatar);
-        collector.checkThat(conversationItemViewHolder.avatar, notNullValue());
         collector.checkThat(conversationItemViewHolder.avatar, is(instanceOf(ImageView.class)));
         collector.checkThat(conversationItemViewHolder.avatar, is(equalTo(avatar)));
     }
 
     @Test
-    public void constructorNameViewTest() {
+    public void constructorNameView() {
         TextView nameTextView = (TextView) view.findViewById(R.id.ko__name);
 
         verify(view, times(2)).findViewById(R.id.ko__name);
-        collector.checkThat(conversationItemViewHolder.name, notNullValue());
         collector.checkThat(conversationItemViewHolder.name, is(instanceOf(TextView.class)));
         collector.checkThat(conversationItemViewHolder.name, is(equalTo(nameTextView)));
     }
 
     @Test
-    public void constructorTimeViewTest() {
+    public void constructorTimeView() {
         TextView timeTextView = (TextView) view.findViewById(R.id.ko__time);
 
         verify(view, times(2)).findViewById(R.id.ko__time);
-        collector.checkThat(conversationItemViewHolder.time, notNullValue());
         collector.checkThat(conversationItemViewHolder.time, is(instanceOf(TextView.class)));
         collector.checkThat(conversationItemViewHolder.time, is(equalTo(timeTextView)));
     }
 
     @Test
-    public void constructorSubjectViewTest() {
+    public void constructorSubjectView() {
         TextView subjectView = (TextView) view.findViewById(R.id.ko__subject);
 
         verify(view, times(2)).findViewById(R.id.ko__subject);
-        collector.checkThat(conversationItemViewHolder.subject, notNullValue());
         collector.checkThat(conversationItemViewHolder.subject, is(instanceOf(TextView.class)));
         collector.checkThat(conversationItemViewHolder.subject, is(equalTo(subjectView)));
     }
 
     @Test
-    public void constructorLayoutViewTest() {
+    public void constructorLayoutView() {
         RelativeLayout rLayout = (RelativeLayout) view.findViewById(R.id.layout);
 
         verify(view, times(2)).findViewById(R.id.layout);
-        collector.checkThat(conversationItemViewHolder.layout, notNullValue());
         collector.checkThat(conversationItemViewHolder.layout, is(instanceOf(RelativeLayout.class)));
         collector.checkThat(conversationItemViewHolder.layout, is(equalTo(rLayout)));
     }
 
     @Test
-    public void constructorUnreadCounterTest() {
+    public void constructorUnreadCounter() {
         TextView unreadCounter = (TextView) view.findViewById(R.id.ko__unread_counter);
 
         verify(view, times(2)).findViewById(R.id.ko__unread_counter);
-        collector.checkThat(conversationItemViewHolder.unreadCounter, notNullValue());
         collector.checkThat(conversationItemViewHolder.unreadCounter, is(instanceOf(TextView.class)));
         collector.checkThat(conversationItemViewHolder.unreadCounter, is(equalTo(unreadCounter)));
     }
 
     @Test
-    public void constructorTypingProgressTest() {
+    public void constructorTypingProgress() {
         ImageView typingProgress = (ImageView) view.findViewById(R.id.ko__typing_progress_loader);
 
         verify(view, times(2)).findViewById(R.id.ko__typing_progress_loader);
-        collector.checkThat(conversationItemViewHolder.typingLoader, notNullValue());
         collector.checkThat(conversationItemViewHolder.typingLoader, is(instanceOf(ImageView.class)));
         collector.checkThat(conversationItemViewHolder.typingLoader, is(equalTo(typingProgress)));
     }
 
     @Test
-    public void constructorSubjectLineTest() {
+    public void constructorSubjectLine() {
         View subjectLine = view.findViewById(R.id.ko__subject_line);
 
         verify(view, times(2)).findViewById(R.id.ko__subject_line);
-        collector.checkThat(conversationItemViewHolder.subjectLine, notNullValue());
         collector.checkThat(conversationItemViewHolder.subjectLine, is(instanceOf(View.class)));
         collector.checkThat(conversationItemViewHolder.subjectLine, is(equalTo(subjectLine)));
     }
-
-
 }
