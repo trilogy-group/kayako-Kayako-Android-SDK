7727e143904f7f9ffa06120b0abcd54a2666c6c0
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-10 02:18:20 +0400

    applied review suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/ListItemRecyclerViewAdapterTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/ListItemRecyclerViewAdapterTest.java
@@ -12,9 +12,10 @@ import com.kayako.sdk.android.k5.common.adapter.loadmorelist.LoadingViewHolder;
 import java.lang.reflect.Field;
 import java.util.List;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 
 import org.mockito.InjectMocks;
@@ -22,41 +23,45 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
-import static junit.framework.Assert.*;
-import static org.powermock.api.mockito.PowerMockito.*;
+import static junit.framework.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({LayoutInflater.class})
 public class ListItemRecyclerViewAdapterTest {
 
-    @Mock
-    ListItemRecyclerViewAdapter.OnListItemClickListener onListItemClickListener;
-
-    @Mock
-    ListItemRecyclerViewAdapter.OnHeaderItemClickListener onHeaderItemClickListener;
-
-    @Mock
-    View mockView;
-
-    @Mock
-    List<BaseListItem> baseListItems;
-    @Mock
-    LayoutInflater mockInflater;
-
-    @Mock
-    ViewGroup mockParent;
-
     private MockContext mockContext;
 
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Mock
+    private ListItemRecyclerViewAdapter.OnListItemClickListener onListItemClickListener;
+
+    @Mock
+    private ListItemRecyclerViewAdapter.OnHeaderItemClickListener onHeaderItemClickListener;
+
+    @Mock
+    private View mockView;
+
+    @Mock
+    private List<BaseListItem> baseListItems;
+
+    @Mock
+    private LayoutInflater mockInflater;
+
+    @Mock
+    private ViewGroup mockParent;
+
     @InjectMocks
-    ListItemRecyclerViewAdapter listItemRecyclerViewAdapter;
+    private ListItemRecyclerViewAdapter listItemRecyclerViewAdapter;
 
     @Before
     public void setUp() {
@@ -88,9 +93,8 @@ public class ListItemRecyclerViewAdapterTest {
 
         RecyclerView.ViewHolder viewHolder = listItemRecyclerViewAdapter.onCreateViewHolder(mockParent, ListType.HEADER_ITEM);
 
-        assertNotNull(viewHolder);
-        assertThat(viewHolder, is(CoreMatchers.instanceOf(HeaderViewHolder.class)));
-        assertEquals(viewHolder.itemView, mockView);
+        collector.checkThat(viewHolder, is(instanceOf(HeaderViewHolder.class)));
+        collector.checkThat(viewHolder.itemView, is(equalTo(mockView)));
     }
 
     @Test
@@ -101,9 +105,8 @@ public class ListItemRecyclerViewAdapterTest {
 
         RecyclerView.ViewHolder viewHolder = listItemRecyclerViewAdapter.onCreateViewHolder(mockParent, ListType.LIST_ITEM);
 
-        assertNotNull(viewHolder);
-        assertThat(viewHolder, is(CoreMatchers.instanceOf(ListItemViewHolder.class)));
-        assertEquals(viewHolder.itemView, mockView);
+        collector.checkThat(viewHolder, is(instanceOf(ListItemViewHolder.class)));
+        collector.checkThat(viewHolder.itemView, is(equalTo(mockView)));
 
     }
 
@@ -115,10 +118,8 @@ public class ListItemRecyclerViewAdapterTest {
 
         RecyclerView.ViewHolder viewHolder = listItemRecyclerViewAdapter.onCreateViewHolder(mockParent, ListType.LOADING_ITEM);
 
-        assertNotNull(viewHolder);
-        assertThat(viewHolder, is(CoreMatchers.instanceOf(LoadingViewHolder.class)));
-        assertEquals(viewHolder.itemView, mockView);
-
+        collector.checkThat(viewHolder, is(instanceOf(LoadingViewHolder.class)));
+        collector.checkThat(viewHolder.itemView, is(equalTo(mockView)));
     }
 
     @Test
@@ -127,6 +128,6 @@ public class ListItemRecyclerViewAdapterTest {
         when(baseListItems.get(0)).thenReturn(mock(BaseListItem.class));
         listItemRecyclerViewAdapter.setData(baseListItems);
         int res = listItemRecyclerViewAdapter.getItemViewType(position);
-        assertThat(res, is(equalTo(listItemRecyclerViewAdapter.getData().get(position).getItemType())));
+        collector.checkThat(res, is(equalTo(listItemRecyclerViewAdapter.getData().get(position).getItemType())));
     }
 }
