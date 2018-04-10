1d36b06a3610208d48a489da98f48c949e1f2a22
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-11 03:36:21 +0400

    Applied suggestions, fixed build

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
@@ -12,18 +12,16 @@ import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.when;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class HeaderViewHolderTest {
@@ -47,17 +45,21 @@ public class HeaderViewHolderTest {
 
     @Test
     public void constructorMView() {
+        //Act
         verify(view, times(1)).findViewById(eq(R.id.ko__list_header_title));
 
+        //Assert
         collector.checkThat(headerViewHolder.mView, is(instanceOf(View.class)));
         collector.checkThat(headerViewHolder.mView, is(equalTo(view)));
     }
 
     @Test
     public void constructorMTitle() {
+        //Act
         TextView textViewMTitle = (TextView) view.findViewById(R.id.ko__list_header_title);
-        verify(view, times(2)).findViewById(anyInt());
+        verify(view, times(2)).findViewById(eq(R.id.ko__list_header_title));
 
+        //Assert
         collector.checkThat(headerViewHolder.mTitle, is(instanceOf(TextView.class)));
         collector.checkThat(headerViewHolder.mTitle, is(equalTo(textViewMTitle)));
     }
