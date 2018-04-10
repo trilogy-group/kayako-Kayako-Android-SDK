0fb69cc2290d47511141ee2495edb3c68047cc50
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-11 00:54:31 +0400

    removed wrong import

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/sectioninfolist/SectionInfoViewHolderTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/sectioninfolist/SectionInfoViewHolderTest.java
@@ -11,13 +11,13 @@ import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,20 +31,22 @@ public class SectionInfoViewHolderTest {
     public final ErrorCollector collector = new ErrorCollector();
 
     @Mock
-    View view;
+    private View view;
 
     @Mock
-    TextView textView;
+    private TextView textView;
 
     @Before
     public void setUp() {
-        when(view.findViewById(anyInt())).thenReturn(textView);
+        when(view.findViewById(eq(R.id.ko__section_title))).thenReturn(textView);
+        when(view.findViewById(eq(R.id.ko__section_description))).thenReturn(textView);
         sectionInfoViewHolder = new SectionInfoViewHolder(view);
     }
 
     @Test
     public void constructorMViewTest() {
-        verify(view, times(2)).findViewById(anyInt());
+        verify(view, times(1)).findViewById(eq(R.id.ko__section_title));
+        verify(view, times(1)).findViewById(eq(R.id.ko__section_description));
 
         collector.checkThat(sectionInfoViewHolder.mView, is(instanceOf(View.class)));
         collector.checkThat(sectionInfoViewHolder.mView, is(equalTo(view)));
@@ -53,7 +55,8 @@ public class SectionInfoViewHolderTest {
     @Test
     public void constructorMTitleTest() {
         TextView textViewMTitle = (TextView) view.findViewById(R.id.ko__section_title);
-        verify(view, times(3)).findViewById(anyInt());
+        verify(view, times(2)).findViewById(eq(R.id.ko__section_title));
+        verify(view, times(1)).findViewById(eq(R.id.ko__section_description));
 
         collector.checkThat(sectionInfoViewHolder.title, is(instanceOf(TextView.class)));
         collector.checkThat(sectionInfoViewHolder.title, is(equalTo(textViewMTitle)));
@@ -62,7 +65,8 @@ public class SectionInfoViewHolderTest {
     @Test
     public void constructorTest() {
         TextView textViewMSubTitle = (TextView) view.findViewById(R.id.ko__section_description);
-        verify(view, times(3)).findViewById(anyInt());
+        verify(view, times(2)).findViewById(eq(R.id.ko__section_description));
+        verify(view, times(1)).findViewById(eq(R.id.ko__section_title));
 
         assertThat(sectionInfoViewHolder.description, is(instanceOf(TextView.class)));
         assertThat(sectionInfoViewHolder.description, is(equalTo(textViewMSubTitle)));
