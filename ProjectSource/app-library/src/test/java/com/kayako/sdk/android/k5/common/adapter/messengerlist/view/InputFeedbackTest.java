4d58df5ce28811314a5f47a5fa0004fed92526fa
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 02:57:03 +0530

    Updated unit test for InputFeedback

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/InputFeedbackTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/InputFeedbackTest.java
@@ -3,20 +3,19 @@ package com.kayako.sdk.android.k5.common.adapter.messengerlist.view;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
 public class InputFeedbackTest {
 
-    private final static String GOOD = "GOOD";
-    private final static String BAD = "BAD";
+    private static final String GOOD = "GOOD";
+    private static final String BAD = "BAD";
 
     @Rule
     public final ErrorCollector errorCollector = new ErrorCollector();
 
     @Test
-    public void enumValue(){
-        errorCollector.checkThat(InputFeedback.RATING.GOOD.name(), is(equalTo(GOOD)));
-        errorCollector.checkThat(InputFeedback.RATING.BAD.name(), is(equalTo(BAD)));
+    public void enumValue() {
+        errorCollector.checkThat(InputFeedback.RATING.GOOD.name(), is(GOOD));
+        errorCollector.checkThat(InputFeedback.RATING.BAD.name(), is(BAD));
     }
 }
