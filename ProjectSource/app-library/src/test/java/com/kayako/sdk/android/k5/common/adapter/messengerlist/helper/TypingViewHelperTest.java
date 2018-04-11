3c09ab533a0c92177d62361e65ad6f0718d80cee
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-11 12:08:15 +0530

    Unit test for TypingViewHelper

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/TypingViewHelperTest.java
@@ -0,0 +1,56 @@
+package com.kayako.sdk.android.k5.common.adapter.messengerlist.helper;
+
+import com.kayako.sdk.android.k5.common.adapter.messengerlist.view.TypingListItem;
+import com.kayako.sdk.android.k5.messenger.data.conversation.viewmodel.UserViewModel;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+public class TypingViewHelperTest {
+
+    private static final String AVATAR = "avatar";
+    private static final String FULL_NAME = "full_name";
+    private final long lastActiveAt = 1_000L;
+    private UserViewModel userViewModel;
+    private TypingViewHelper typingViewHelper;
+
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
+    @Before
+    public void setUp() {
+        userViewModel = new UserViewModel(AVATAR, FULL_NAME, lastActiveAt);
+        typingViewHelper = new TypingViewHelper();
+    }
+
+    @Test
+    public void validObjectCreation(){
+        errorCollector.checkThat(typingViewHelper, notNullValue());
+    }
+
+    @Test
+    public void whenIsTypingFalseThenSetTypingStatusReturnFalse() {
+        errorCollector.checkThat(typingViewHelper.setTypingStatus(userViewModel, false), is(false));
+    }
+
+    @Test
+    public void whenIsTypingTrueThenSetTypingStatusReturnTrue() {
+        errorCollector.checkThat(typingViewHelper.setTypingStatus(userViewModel, true), is(true));
+    }
+
+    @Test
+    public void whenAgentTypingFalseThenGetTypingViewReturnEmptyList() {
+        errorCollector.checkThat(typingViewHelper.getTypingViews().isEmpty(), is(true));
+    }
+
+    @Test
+    public void whenAgentTypingStatusTrueThenGetTypingViewReturnListWithTypingListItem() {
+        typingViewHelper.setTypingStatus(userViewModel, true);
+        errorCollector.checkThat(typingViewHelper.getTypingViews().isEmpty(), is(false));
+        errorCollector.checkThat(typingViewHelper.getTypingViews().get(0), is(instanceOf(TypingListItem.class)));
+    }
+}
