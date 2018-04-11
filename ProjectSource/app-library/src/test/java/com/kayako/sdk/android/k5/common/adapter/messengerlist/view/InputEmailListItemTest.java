03466ba96d75bf2b426e151b1a40bf696cd6ffd3
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-11 10:48:22 +0530

    unit test case for InputEmailListItem

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/InputEmailListItemTest.java
@@ -0,0 +1,49 @@
+package com.kayako.sdk.android.k5.common.adapter.messengerlist.view;
+
+import com.kayako.sdk.android.k5.common.adapter.messengerlist.MessengerListType;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InputEmailListItemTest {
+
+    private static final String SUBMITTED_EMAIL  = "abc@xyz.com";
+    private InputEmailListItem emailListItem;
+
+    @Mock
+    private InputEmailListItem.OnClickSubmitListener onClickSubmitListener;
+
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
+    @Before
+    public void setUp(){
+        emailListItem = new InputEmailListItem(SUBMITTED_EMAIL);
+    }
+
+    @Test
+    public void whenValidParamsInConstructorGivenThenObjectCreated() {
+        errorCollector.checkThat(emailListItem.getItemType(), is(equalTo(MessengerListType.INPUT_FIELD_EMAIL)));
+        errorCollector.checkThat(emailListItem.getSubmittedValue(), is(equalTo(SUBMITTED_EMAIL)));
+    }
+
+    @Test
+    public void whenValidParamsConstructorThenObjectCreated() {
+        InputEmailListItem emailListItemLocal = new InputEmailListItem(onClickSubmitListener);
+        errorCollector.checkThat(emailListItemLocal.getOnClickSubmitListener(), is(onClickSubmitListener));
+        errorCollector.checkThat(emailListItemLocal.getItemType(), is(equalTo(MessengerListType.INPUT_FIELD_EMAIL)));
+    }
+
+    @Test
+    public void getContents() {
+        errorCollector.checkThat(emailListItem.getContents().size(), is(2));
+        errorCollector.checkThat(emailListItem.getContents().get("getSubmittedValue"), is(equalTo(SUBMITTED_EMAIL)));
+    }
+}
