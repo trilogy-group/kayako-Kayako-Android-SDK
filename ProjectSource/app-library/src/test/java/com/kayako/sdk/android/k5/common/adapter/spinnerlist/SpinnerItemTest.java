9ee4ff95639423555632027886553d4f6a255a08
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-11 12:43:42 +0400

    Applied suggestions

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/spinnerlist/SpinnerItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/spinnerlist/SpinnerItemTest.java
@@ -14,29 +14,16 @@ import static org.junit.Assert.assertTrue;
 @RunWith(MockitoJUnitRunner.class)
 public class SpinnerItemTest {
 
-    @Mock
-    private Resource resource;
-
     private static final SpinnerItem spinnerItem = new SpinnerItem();
-
     private static final String LABEL = "labelForTest";
 
-    @Test
-    public void getResourceTest() {
-        spinnerItem.setResource(resource);
-        assertThat(spinnerItem.getResource(), is(resource));
-    }
+    @Mock
+    private Resource resource;
 
     @Test
     public void setResourceTest() {
         spinnerItem.setResource(resource);
         assertThat(spinnerItem.getResource(), is(resource));
-    }
-
-    @Test
-    public void getLabel() {
-        spinnerItem.setLabel(LABEL);
-        assertThat(spinnerItem.getLabel(), is(LABEL));
     }
 
     @Test
