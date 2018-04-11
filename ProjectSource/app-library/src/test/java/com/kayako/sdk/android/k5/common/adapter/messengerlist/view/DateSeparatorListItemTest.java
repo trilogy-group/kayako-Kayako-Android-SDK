7299212fde4bca6f3781c3eca1ee5e6eaa4f7b8d
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 00:36:03 +0530

    Unit test case for DateSeparatorListItem

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/DateSeparatorListItemTest.java
@@ -0,0 +1,44 @@
+package com.kayako.sdk.android.k5.common.adapter.messengerlist.view;
+
+import com.kayako.sdk.android.k5.common.adapter.messengerlist.MessengerListType;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class DateSeparatorListItemTest {
+
+    private long timeInMilliseconds;
+    private DateSeparatorListItem separatorListItem;
+
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
+    @Before
+    public void setUp(){
+        timeInMilliseconds = 10_000L;
+        separatorListItem = new DateSeparatorListItem(timeInMilliseconds);
+    }
+
+    @Test
+    public void whenValidParamsConstructorThenObjectCreated() {
+        errorCollector.checkThat(separatorListItem.getTimeInMilliseconds(), is(timeInMilliseconds));
+        errorCollector.checkThat(separatorListItem.getItemType(), is(MessengerListType.DATE_SEPARATOR));
+    }
+
+    @Test
+    public void setTimeInMilliseconds(){
+        final long newTime = 22_222L;
+        separatorListItem.setTimeInMilliseconds(newTime);
+        errorCollector.checkThat(separatorListItem.getTimeInMilliseconds(), is(newTime));
+    }
+
+    @Test
+    public void getContents() {
+        errorCollector.checkThat(separatorListItem.getContents().size(), is(1));
+        errorCollector.checkThat(separatorListItem.getContents().get("timeInMilliseconds"),
+                                is(equalTo(String.valueOf(timeInMilliseconds))));
+    }
+}
