8874ec6e0a04f0862c1705ceb26000ff52569521
Samir Aghayarov <samir.aghayarov@aurea.com>
2018-04-11 19:28:14 +0400

    Merge branch 'develop' into feature/KAYAKO-714
    # Conflicts:
    #	ProjectSource/app-library/build.gradle
    #	ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/searchsectionlist/SearchSectionViewHolderTest.java
    #	ProjectSource/gradle.properties

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/list/HeaderViewHolderTest.java
@@ -12,7 +12,7 @@ import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.Matchers.eq;
