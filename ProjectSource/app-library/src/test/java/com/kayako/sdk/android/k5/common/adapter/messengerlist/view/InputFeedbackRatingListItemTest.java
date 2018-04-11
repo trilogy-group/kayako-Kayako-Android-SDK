ca0919516fca464c20762f05fa44414b81db1724
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-11 11:05:51 +0530

    Unit test for InputFeedbackRatingListItem

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/InputFeedbackRatingListItemTest.java
@@ -0,0 +1,82 @@
+package com.kayako.sdk.android.k5.common.adapter.messengerlist.view;
+
+import com.kayako.sdk.android.k5.common.adapter.messengerlist.MessengerListType;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InputFeedbackRatingListItemTest {
+
+    private static final String INSTRUCTION_MESSAGE = "instruction_message";
+    private static final String EXCEPTION_MESSAGE = "Invalid arguments";
+    private static final String SUBMITTED_RATING = "submitted_rating";
+    private InputFeedbackRatingListItem ratingListItem;
+
+    @Mock
+    private InputFeedback.OnSelectRatingListener onSelectRatingListener;
+
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        ratingListItem = new InputFeedbackRatingListItem(INSTRUCTION_MESSAGE, onSelectRatingListener);
+    }
+
+    @Test
+    public void whenValidParamsConstructorThenObjectCreated(){
+        errorCollector.checkThat(ratingListItem.getInstructionMessage(), is(equalTo(INSTRUCTION_MESSAGE)));
+        errorCollector.checkThat(ratingListItem.getOnSelectRatingListener(), is(onSelectRatingListener));
+        errorCollector.checkThat(ratingListItem.getItemType(), is(equalTo(MessengerListType.INPUT_FIELD_FEEDBACK_RATING)));
+    }
+
+    @Test
+    public void whenNullInstructionMessageThenIllegalStateException(){
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage(containsString(EXCEPTION_MESSAGE));
+        InputFeedbackRatingListItem ratingListItemLocal =
+                new InputFeedbackRatingListItem(null, onSelectRatingListener);
+    }
+
+    @Test
+    public void whenNullOnSelectRatingListenerThenException(){
+        InputFeedback.OnSelectRatingListener onSelectRatingListener = null;
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage(containsString(EXCEPTION_MESSAGE));
+        InputFeedbackRatingListItem ratingListItemLocal =
+                new InputFeedbackRatingListItem(INSTRUCTION_MESSAGE, onSelectRatingListener);
+    }
+
+    @Test
+    public void whenValidParamsGivenConstructorThenObjectCreated() {
+        InputFeedbackRatingListItem listItemLocal = new InputFeedbackRatingListItem(INSTRUCTION_MESSAGE, SUBMITTED_RATING);
+        errorCollector.checkThat(listItemLocal.getInstructionMessage(), is(equalTo(INSTRUCTION_MESSAGE)));
+    }
+
+    @Test
+    public void whenNullSubmittedRatingThenIllegalStateException() {
+        String submittedRating = null;
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage(containsString(EXCEPTION_MESSAGE));
+        InputFeedbackRatingListItem listItem = new InputFeedbackRatingListItem(INSTRUCTION_MESSAGE, submittedRating);
+    }
+
+    @Test
+    public void getContents() {
+        InputFeedbackRatingListItem listItem = new InputFeedbackRatingListItem(INSTRUCTION_MESSAGE, SUBMITTED_RATING);
+        errorCollector.checkThat(listItem.getContents().isEmpty(), is(false));
+        errorCollector.checkThat(ratingListItem.getContents().get("instructionMessage"), is(equalTo(INSTRUCTION_MESSAGE)));
+    }
+}
