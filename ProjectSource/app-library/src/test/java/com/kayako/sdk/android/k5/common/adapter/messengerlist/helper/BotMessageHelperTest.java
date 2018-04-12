b10726c2b42e048fb3210e03ffb56cc9c61ebf81
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 10:22:04 +0530

    Updated unit test for BotMessageHelper

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/BotMessageHelperTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/BotMessageHelperTest.java
@@ -1,23 +1,20 @@
 package com.kayako.sdk.android.k5.common.adapter.messengerlist.helper;
 
 import com.kayako.sdk.android.k5.R;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ErrorCollector;
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 
 public class BotMessageHelperTest {
 
-    @Rule
-    public final ErrorCollector errorCollector = new ErrorCollector();
-
     @Test
     public void getBotDrawableForSystemMessage() {
-        errorCollector.checkThat(BotMessageHelper.getBotDrawableForSystemMessage(), is(R.drawable.ko__bot_avatar));
+        assertEquals(R.drawable.ko__bot_avatar,
+                BotMessageHelper.getBotDrawableForSystemMessage());
     }
 
     @Test
     public void getDefaultDrawableForConversation() {
-        errorCollector.checkThat(BotMessageHelper.getDefaultDrawableForConversation(), is(R.drawable.ko__ic_default_agent));
+        assertEquals(R.drawable.ko__ic_default_agent,
+                BotMessageHelper.getDefaultDrawableForConversation());
     }
 }
