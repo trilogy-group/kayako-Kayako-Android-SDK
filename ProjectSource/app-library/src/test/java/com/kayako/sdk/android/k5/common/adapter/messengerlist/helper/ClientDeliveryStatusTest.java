8d25537c8715115bdab2861a0b933b469eaeabf8
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 10:45:41 +0530

    Updated unit test for ClientDeliveryStatus

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/ClientDeliveryStatusTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/ClientDeliveryStatusTest.java
@@ -4,7 +4,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 public class ClientDeliveryStatusTest {
 
@@ -15,8 +14,8 @@ public class ClientDeliveryStatusTest {
     public final ErrorCollector errorCollector = new ErrorCollector();
 
     @Test
-    public void test_enum() {
-        errorCollector.checkThat(ClientDeliveryStatus.SENDING.name(), is(equalTo(SENDING_STRING)));
-        errorCollector.checkThat(ClientDeliveryStatus.FAILED_TO_SEND.name(), is(equalTo(FAILED_TO_SEND_STRING)));
+    public void givenClientDeliveryStatusWhenNameThenExpected() {
+        errorCollector.checkThat(ClientDeliveryStatus.SENDING.name(), is(SENDING_STRING));
+        errorCollector.checkThat(ClientDeliveryStatus.FAILED_TO_SEND.name(), is(FAILED_TO_SEND_STRING));
     }
 }
