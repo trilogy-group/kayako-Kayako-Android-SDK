ca42afe38f5aac99dd41bb05796f899637281dd5
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 10:43:29 +0530

    Updated unit test for UserDecorationHelper

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/UserDecorationHelperTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/helper/UserDecorationHelperTest.java
@@ -3,21 +3,18 @@ package com.kayako.sdk.android.k5.common.adapter.messengerlist.helper;
 import com.kayako.sdk.android.k5.common.adapter.messengerlist.UserDecoration;
 import com.kayako.sdk.helpcenter.user.UserMinimal;
 import com.kayako.sdk.messenger.message.Message;
-import org.junit.Rule;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserDecorationHelperTest {
 
-    private final long userId = 1L;
+    private static final long USER_ID = 1L;
 
     @Mock
     private Message message;
@@ -25,27 +22,29 @@ public class UserDecorationHelperTest {
     @Mock
     private UserMinimal userMinimal;
 
-    @Rule
-    public final ErrorCollector errorCollector = new ErrorCollector();
-
     @Test
     public void whenNullMessageThenReturnNull() {
-       UserDecoration userDecoration = UserDecorationHelper.getUserDecoration(null, userId);
-       errorCollector.checkThat(userDecoration, nullValue());
+       final UserDecoration userDecoration = UserDecorationHelper.getUserDecoration(null, USER_ID);
+       assertNull(userDecoration);
     }
 
     @Test
     public void whenMessageCreatorNullThenNull() {
-        UserDecoration userDecoration = UserDecorationHelper.getUserDecoration(message, userId);
-        errorCollector.checkThat(userDecoration, nullValue());
+        final UserDecoration userDecoration = UserDecorationHelper.getUserDecoration(message, USER_ID);
+        assertNull(userDecoration);
     }
 
     @Test
     public void whenValidParamsThenObjectCreated() {
+        //Arrange
         final String fullName = "full_Name";
         when(message.getCreator()).thenReturn(userMinimal);
         when(message.getCreator().getFullName()).thenReturn(fullName);
-        UserDecoration userDecoration = UserDecorationHelper.getUserDecoration(message, userId);
-        errorCollector.checkThat(userDecoration.getName(), is(equalTo(fullName)));
+
+        //Act
+        final UserDecoration userDecoration = UserDecorationHelper.getUserDecoration(message, USER_ID);
+
+        //Assert
+        assertEquals(fullName, userDecoration.getName());
     }
 }
