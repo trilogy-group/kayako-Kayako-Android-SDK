8a0856147ec4dc91936905079ddb49d6c9877ff5
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 11:09:32 +0530

    Updated unit test for SimpleMessageOtherListItem

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/SimpleMessageOtherListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/view/SimpleMessageOtherListItemTest.java
@@ -7,14 +7,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleMessageOtherListItemTest {
 
-    private Map<String, Object> data = new HashMap<>();
+    private Map<String, String> data = new HashMap<>();
     private long id;
     private static final String MESSAGE = "test message";
     private static final String AVATAR_URL = "/avatarUrl";
@@ -26,59 +25,79 @@ public class SimpleMessageOtherListItemTest {
     public final ErrorCollector errorCollector = new ErrorCollector();
 
     @Before
-    public void setUp(){
+    public void setUp() {
         id = 1L;
         time = 1_234L;
         channel = new ChannelDecoration(1);
-        otherListItem = new SimpleMessageOtherListItem(id, MESSAGE, AVATAR_URL, channel,time, data);
+        otherListItem = new SimpleMessageOtherListItem(id, MESSAGE, AVATAR_URL, channel,time, (Map)data);
     }
 
     @Test
     public void whenValidParamsInConstructorThenObjectCreated() {
-        errorCollector.checkThat(otherListItem.getItemType(), is(equalTo(MessengerListType.SIMPLE_MESSAGE_OTHER)));
+        errorCollector.checkThat(otherListItem.getItemType(), is(MessengerListType.SIMPLE_MESSAGE_OTHER));
         errorCollector.checkThat(otherListItem.getId(), is(id));
-        errorCollector.checkThat(otherListItem.getAvatarUrl(), is(equalTo(AVATAR_URL)));
-        errorCollector.checkThat(otherListItem.getMessage(), is(equalTo(MESSAGE)));
+        errorCollector.checkThat(otherListItem.getAvatarUrl(), is(AVATAR_URL));
+        errorCollector.checkThat(otherListItem.getMessage(), is(MESSAGE));
         errorCollector.checkThat(otherListItem.getChannel(), is(channel));
         errorCollector.checkThat(otherListItem.getTime(), is(time));
-        errorCollector.checkThat(otherListItem.getData(), is(data));
+        errorCollector.checkThat((Map)otherListItem.getData(), is(data));
     }
 
     @Test
     public void setMessage() {
+        //Arrange
         final String newMessage = "new_message";
+
+        //Act
         otherListItem.setMessage(newMessage);
-        errorCollector.checkThat(otherListItem.getMessage(), is(equalTo(newMessage)));
+
+        //Assert
+        errorCollector.checkThat(otherListItem.getMessage(), is(newMessage));
     }
 
     @Test
-    public void setAvatarUrl(){
+    public void setAvatarUrl() {
+        //Arrange
         final String newAvatarUrl = "/avatarUrl_2";
+
+        //Act
         otherListItem.setAvatarUrl(newAvatarUrl);
-        errorCollector.checkThat(otherListItem.getAvatarUrl(), is(equalTo(newAvatarUrl)));
+
+        //Assert
+        errorCollector.checkThat(otherListItem.getAvatarUrl(), is(newAvatarUrl));
     }
 
     @Test
-    public void setTime(){
+    public void setTime() {
+        //Arrange
         final long newTime = 54_321L;
+
+        //Act
         otherListItem.setTime(newTime);
+
+        //Assert
         errorCollector.checkThat(otherListItem.getTime(), is(newTime));
     }
 
     @Test
     public void setChannel() {
+        //Arrange
         final int sourceDrawable = 5;
-        ChannelDecoration channelDecorationLocal = new ChannelDecoration(sourceDrawable);
+        final ChannelDecoration channelDecorationLocal = new ChannelDecoration(sourceDrawable);
+
+        //Act
         otherListItem.setChannel(channelDecorationLocal);
+
+        //Assert
         errorCollector.checkThat(otherListItem.getChannel(), is(not(channel)));
         errorCollector.checkThat(otherListItem.getChannel(), is(channelDecorationLocal));
         errorCollector.checkThat(otherListItem.getChannel().getSourceDrawable(), is(sourceDrawable));
     }
 
     @Test
-    public void getContents(){
-        Map<String, String> contentsMap = otherListItem.getContents();
+    public void getContents() {
+        final Map<String, String> contentsMap = otherListItem.getContents();
         errorCollector.checkThat(contentsMap.size(), is(6));
-        errorCollector.checkThat(contentsMap.get("isNote"), is(equalTo("false")));
+        errorCollector.checkThat(contentsMap.get("isNote"), is("false"));
     }
 }
