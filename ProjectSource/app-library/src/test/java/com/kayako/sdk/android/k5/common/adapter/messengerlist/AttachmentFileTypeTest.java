491f4679c6121158c1b097ba1e051d02a2979043
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-12 12:09:35 +0530

    Updated unit test for AttachmentFileType

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/AttachmentFileTypeTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/adapter/messengerlist/AttachmentFileTypeTest.java
@@ -1,10 +1,11 @@
 package com.kayako.sdk.android.k5.common.adapter.messengerlist;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import java.io.File;
 
@@ -20,16 +21,23 @@ public class AttachmentFileTypeTest {
 
     @Test
     public void givenNullFileWhenConstructorThenAssertionError() {
+        final Matcher<String> nullMatcher = new IsNull<>();
         thrown.expect(AssertionError.class);
-        AttachmentFileType attachmentFileType = new AttachmentFileType(1L, null, CAPTION);
+        thrown.expectMessage(nullMatcher);
+        new AttachmentFileType(1L, null, CAPTION);
     }
 
     @Test
     public void givenValidParamsWhenConstructorThenObjectCreated() {
+        //Arrange
         File file = new File("");
-        AttachmentFileType attachmentFileType = new AttachmentFileType(1L, file, CAPTION);
+
+        //Act
+        final AttachmentFileType attachmentFileType = new AttachmentFileType(1L, file, CAPTION);
+
+        //Assert
         errorCollector.checkThat(attachmentFileType.getId(), is(1L));
-        errorCollector.checkThat(attachmentFileType.getCaption(), is(equalTo(CAPTION)));
+        errorCollector.checkThat(attachmentFileType.getCaption(), is(CAPTION));
         errorCollector.checkThat(attachmentFileType.getThumbnailFile(), is(file));
         errorCollector.checkThat(attachmentFileType.getType(), is(AttachmentFileType.TYPE.FILE));
     }
