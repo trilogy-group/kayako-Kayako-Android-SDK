2536f0ae8e646cb21d4ee64f7cd8aee5529f1079
intipadillaaurea <38227218+intipadillaaurea@users.noreply.github.com>
2018-04-13 11:20:59 -0400

    format corrections
    Format corrections

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/utils/file/FileFormatUtilTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/utils/file/FileFormatUtilTest.java
@@ -5,22 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FileFormatUtilTest {
-    public static final Long TEST_FILE_SIZE_BYTES= 13245678L;
+    public static final Long TEST_FILE_SIZE_BYTES = 13_245_678L;
     public static final String EXPECTED_OUTPUT = "12 MB";
     public static final String EXPECTED_OUTPUT_ON_NULL = "-";
     public static final String EXPECTED_OUTPUT_ON_SMALL_SIZE = "0 KB";
+
     @Test
-    public void verifyIfGettingCorrectSizeConversion(){
+    public void verifyIfGettingCorrectSizeConversion() {
         assertEquals(EXPECTED_OUTPUT, FileFormatUtil.formatFileSize(TEST_FILE_SIZE_BYTES));
     }
 
     @Test
-    public void verifyIfGettingSlashOnNullParameter(){
+    public void verifyIfGettingSlashOnNullParameter() {
         assertEquals(EXPECTED_OUTPUT_ON_NULL, FileFormatUtil.formatFileSize(null));
     }
 
     @Test
-    public void verifyGettingZeroOnLessThan1024Bytes(){
-        assertEquals(EXPECTED_OUTPUT_ON_SMALL_SIZE,FileFormatUtil.formatFileSize(12L));
+    public void verifyGettingZeroOnLessThan1024Bytes() {
+        assertEquals(EXPECTED_OUTPUT_ON_SMALL_SIZE, FileFormatUtil.formatFileSize(12L));
     }
 }
