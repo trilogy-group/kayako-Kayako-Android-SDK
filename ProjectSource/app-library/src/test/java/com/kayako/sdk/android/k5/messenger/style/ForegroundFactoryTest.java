045edf1b8ff8562726c7c738b9e6c018864ab282
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-15 14:04:07 +0530

    Unit test for ForegroundFactory

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/messenger/style/ForegroundFactoryTest.java
@@ -0,0 +1,60 @@
+package com.kayako.sdk.android.k5.messenger.style;
+
+import com.kayako.sdk.android.k5.messenger.style.type.BlankForground;
+import com.kayako.sdk.android.k5.messenger.style.type.Foreground;
+import com.kayako.sdk.android.k5.messenger.style.type.Texture;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import java.util.LinkedList;
+import java.util.List;
+import com.kayako.sdk.android.k5.messenger.style.ForegroundFactory.ForegroundOption;
+
+public class ForegroundFactoryTest {
+
+    private final List<ForegroundOption> foreGroundList =
+            new LinkedList<>();
+
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
+    @Before
+    public void setUp() {
+        foreGroundList.add(ForegroundOption.CONFETTI);
+        foreGroundList.add(ForegroundOption.DOTS);
+        foreGroundList.add(ForegroundOption.CONSTELLATION);
+        foreGroundList.add(ForegroundOption.SAND);
+        foreGroundList.add(ForegroundOption.CHEERIOS);
+        foreGroundList.add(ForegroundOption.MOSAIC);
+        foreGroundList.add(ForegroundOption.POLKA);
+        foreGroundList.add(ForegroundOption.STARS);
+        foreGroundList.add(ForegroundOption.NACHOS);
+        foreGroundList.add(ForegroundOption.ZIGZAG);
+    }
+
+    @Test
+    public void textureForeGroundOption() {
+        for (ForegroundOption foregroundOption : foreGroundList) {
+            final Foreground foreground = ForegroundFactory.getForeground(foregroundOption);
+            errorCollector.checkThat(foreground, instanceOf(Texture.class));
+            errorCollector.checkThat(foreground.getType(), is(Foreground.ForegroundType.TEXTURE));
+        }
+    }
+
+    @Test
+    public void whenNoTextureThenBlankForeGround() {
+        final Foreground foreground = ForegroundFactory.getForeground(ForegroundOption.NO_TEXTURE);
+        errorCollector.checkThat(foreground, instanceOf(BlankForground.class));
+        errorCollector.checkThat(foreground.getType(), is(Foreground.ForegroundType.NONE));
+    }
+
+    @Test
+    public void whenNullThenGetForeGroundReturnNull() {
+        final Foreground foreground = ForegroundFactory.getForeground(null);
+        errorCollector.checkThat(foreground, nullValue());
+    }
+}
