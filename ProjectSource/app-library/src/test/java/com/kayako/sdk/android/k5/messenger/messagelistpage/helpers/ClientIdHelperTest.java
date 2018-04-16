4d968e563308bafc3145b60ee5ae9b1516fd59e8
Sibani Sankar Pradhan <sibhani.sankarpradhan@aurea.com>
2018-04-16 08:21:26 +0530

    Unit test for ClientIdHelper

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/messenger/messagelistpage/helpers/ClientIdHelperTest.java
@@ -0,0 +1,26 @@
+package com.kayako.sdk.android.k5.messenger.messagelistpage.helpers;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import static com.kayako.sdk.android.k5.messenger.messagelistpage.helpers.ClientIdHelper.MessageType;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ClientIdHelperTest {
+
+    @Rule
+    public final ErrorCollector errorCollector = new ErrorCollector();
+
+    @Test
+    public void generateClientId() {
+        final String android = "android";
+        final ClientIdHelper idHelperFirst = new ClientIdHelper();
+        final ClientIdHelper idHelperSecond = new ClientIdHelper();
+        String firstClientId = idHelperFirst.generateClientId(MessageType.MESSAGE);
+        String secondClientId = idHelperSecond.generateClientId(MessageType.ATTACHMENT);
+        errorCollector.checkThat(firstClientId.contains(MessageType.MESSAGE.name()), is(true));
+        errorCollector.checkThat(secondClientId.contains(MessageType.ATTACHMENT.name()), is(true));
+        errorCollector.checkThat(firstClientId.contains(android), is(true));
+        errorCollector.checkThat(secondClientId.contains(android), is(true));
+    }
+}
