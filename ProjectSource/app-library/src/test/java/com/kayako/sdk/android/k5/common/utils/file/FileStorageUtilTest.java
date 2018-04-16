df421aae9133c7f6d3ac7fb51704424c9b15fb57
intipadillaaurea <38227218+intipadillaaurea@users.noreply.github.com>
2018-04-16 01:17:08 -0400

    Format Corrections
    Format corrections

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/utils/file/FileStorageUtilTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/utils/file/FileStorageUtilTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
@@ -24,6 +23,10 @@ import static junit.framework.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.stub;
+import static org.powermock.api.mockito.PowerMockito.method;
 import static org.powermock.api.mockito.PowerMockito.doReturn;
 
 @RunWith(PowerMockRunner.class)
@@ -34,9 +37,12 @@ import static org.powermock.api.mockito.PowerMockito.doReturn;
         InputStream.class
 })
 public class FileStorageUtilTest {
-    private final static String TEST_FILE_NAME = "Not Valid File Name $%&$%/(.txt";
-    private final static String CORRECT_TEST_FILE_NAME = "NotValidFileName.txt";
+    private static final String TEST_FILE_NAME = "Not Valid File Name $%&$%/(.txt";
+    private static final String CORRECT_TEST_FILE_NAME = "NotValidFileName.txt";
     private static final String ATTACHMENT_NAME_PREFIX = "attachment__";
+    private static final String JPG_MIME_TYPE = "image/jpg";
+    private static final String TEST_FILE_EXTENSION = "txt";
+    private static final String IMAGE_FILE_EXTENSION = "jpg";
     private static File testFile;
 
     @Mock
@@ -57,13 +63,13 @@ public class FileStorageUtilTest {
     }
 
     @Test
-    public void verifyIfPurifyFileName(){
+    public void verifyIfPurifyFileName() {
         String resultedFileName = FileStorageUtil.purify(TEST_FILE_NAME);
-        assertEquals(CORRECT_TEST_FILE_NAME,resultedFileName);
+        assertEquals(CORRECT_TEST_FILE_NAME, resultedFileName);
     }
 
     @Test
-    public void verifyIfReturnsCorrectFilePath(){
+    public void verifyIfReturnsCorrectFilePath() {
         String returnedFilePath = FileStorageUtil.getFilePath(testFile);
         assertTrue(new File(returnedFilePath).exists());
     }
@@ -76,8 +82,9 @@ public class FileStorageUtilTest {
 
     @Test
     public void verifyIfReturningCorrectFileExtension() {
-        PowerMockito.mockStatic(MimeTypeMap.class);
-        PowerMockito.when(MimeTypeMap.getFileExtensionFromUrl(testFile.getAbsolutePath())).thenReturn("txt");
+        mockStatic(MimeTypeMap.class);
+        when(MimeTypeMap.getFileExtensionFromUrl(testFile.getAbsolutePath()))
+                .thenReturn(TEST_FILE_EXTENSION);
         String returnedFileExtension = FileStorageUtil.getFileExtension(testFile);
         assertEquals("txt", returnedFileExtension);
     }
@@ -85,14 +92,17 @@ public class FileStorageUtilTest {
     @Test
     public void verifyIfVerifyIfGetMimeTypeCorrectly() throws Exception {
         MimeTypeMap mockHelper = Whitebox.invokeConstructor(MimeTypeMap.class);
-        PowerMockito.mockStatic(MimeTypeMap.class);
-        PowerMockito.when(MimeTypeMap.getFileExtensionFromUrl(testFile.getAbsolutePath())).thenReturn("jpg");
-        PowerMockito.when(MimeTypeMap.getSingleton()).thenReturn(mockHelper);
-        PowerMockito.stub(PowerMockito.method(MimeTypeMap.class, "getMimeTypeFromExtension", String.class)).toReturn("image/jpg");
-        PowerMockito.mockStatic(TextUtils.class);
-        PowerMockito.stub(PowerMockito.method(TextUtils.class, "isEmpty", String.class)).toReturn(false);
+        mockStatic(MimeTypeMap.class);
+        when(MimeTypeMap.getFileExtensionFromUrl(testFile.getAbsolutePath()))
+                .thenReturn(IMAGE_FILE_EXTENSION);
+        when(MimeTypeMap.getSingleton()).thenReturn(mockHelper);
+        stub(method(MimeTypeMap.class, "getMimeTypeFromExtension", String.class))
+                .toReturn(JPG_MIME_TYPE);
+        mockStatic(TextUtils.class);
+        stub(method(TextUtils.class, "isEmpty", String.class))
+                .toReturn(false);
         String returnedMimeType = FileStorageUtil.getMimeType(testFile);
-        assertEquals("image/jpg", returnedMimeType);
+        assertEquals(JPG_MIME_TYPE, returnedMimeType);
     }
 
     @Test
@@ -103,37 +113,43 @@ public class FileStorageUtilTest {
 
     @Test
     public void verifyIfSaveFileCorrectly() throws Exception {
-        PowerMockito.mockStatic(Uri.class);
+        mockStatic(Uri.class);
         Uri uri = mock(Uri.class);
-        PowerMockito.when(Uri.class, "parse", anyString()).thenReturn(uri);
+        when(Uri.class, "parse", anyString()).thenReturn(uri);
         doReturn(contentResolver).when(context).getContentResolver();
-        PowerMockito.when(inputStream, "read",any()).thenReturn(-1);
-        PowerMockito.when(contentResolver,"openInputStream",uri).thenReturn(inputStream);
-        PowerMockito.when(context,"getFileStreamPath",anyString()).thenReturn(testFile);
-        File saveFile = FileStorageUtil.saveFile(context, Uri.parse(new java.net.URI("").toString()),CORRECT_TEST_FILE_NAME);
-        assertTrue(saveFile.exists());
+        when(inputStream, "read", any()).thenReturn(-1);
+        when(contentResolver, "openInputStream", uri).thenReturn(inputStream);
+        when(context, "getFileStreamPath", anyString()).thenReturn(testFile);
+        File saveFile = FileStorageUtil
+                .saveFile(context, Uri.parse(new java.net.URI("").toString()), CORRECT_TEST_FILE_NAME);
+        assertTrue(saveFile.isFile());
     }
 
     @Test
     public void verifyIfDeleteFilesCorrectly() throws Exception {
-        PowerMockito.when(context,"fileList").thenReturn(new String[]{ATTACHMENT_NAME_PREFIX+CORRECT_TEST_FILE_NAME});
-        assertTrue(FileStorageUtil.deleteSavedFiles(context));
+        when(context, "fileList")
+                .thenReturn(new String[]{ATTACHMENT_NAME_PREFIX + CORRECT_TEST_FILE_NAME});
+        boolean deleted = FileStorageUtil.deleteSavedFiles(context);
+        assertTrue(deleted);
     }
 
     @Test
     public void verifyIfValidatingImageCorrectly() throws Exception {
         MimeTypeMap mockHelper = Whitebox.invokeConstructor(MimeTypeMap.class);
-        PowerMockito.mockStatic(MimeTypeMap.class);
-        PowerMockito.when(MimeTypeMap.getFileExtensionFromUrl(testFile.getAbsolutePath())).thenReturn("jpg");
-        PowerMockito.when(MimeTypeMap.getSingleton()).thenReturn(mockHelper);
-        PowerMockito.stub(PowerMockito.method(MimeTypeMap.class, "getMimeTypeFromExtension", String.class)).toReturn("image/jpg");
-        PowerMockito.mockStatic(TextUtils.class);
-        PowerMockito.stub(PowerMockito.method(TextUtils.class, "isEmpty", String.class)).toReturn(false);
+        mockStatic(MimeTypeMap.class);
+        when(MimeTypeMap.getFileExtensionFromUrl(testFile.getAbsolutePath()))
+                .thenReturn(IMAGE_FILE_EXTENSION);
+        when(MimeTypeMap.getSingleton()).thenReturn(mockHelper);
+        stub(method(MimeTypeMap.class, "getMimeTypeFromExtension", String.class))
+                .toReturn(JPG_MIME_TYPE);
+        mockStatic(TextUtils.class);
+        stub(method(TextUtils.class, "isEmpty", String.class))
+                .toReturn(false);
         assertTrue(FileStorageUtil.isImage(testFile));
     }
 
     @Test
-    public void verifyIfVerifyingExistingCorrectly(){
+    public void verifyIfVerifyingExistingCorrectly() {
         assertTrue(FileStorageUtil.isExisting(testFile));
     }
 }
