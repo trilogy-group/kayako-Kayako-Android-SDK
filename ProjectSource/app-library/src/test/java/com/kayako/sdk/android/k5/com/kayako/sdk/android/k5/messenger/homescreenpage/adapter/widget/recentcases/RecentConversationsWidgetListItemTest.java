618016743f970f93aec2d8e208055b37e94719a6
pedrohvcoelho <38250790+pedrohvcoelho@users.noreply.github.com>
2018-04-18 20:31:05 -0300

    Update RecentConversationsWidgetListItemTest.java

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/com/kayako/sdk/android/k5/messenger/homescreenpage/adapter/widget/recentcases/RecentConversationsWidgetListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/com/kayako/sdk/android/k5/messenger/homescreenpage/adapter/widget/recentcases/RecentConversationsWidgetListItemTest.java
@@ -1,4 +1,4 @@
-package com.kayako.sdk.android.k5.com.kayako.sdk.android.k5.messenger.homescreenpage.adapter.widget.recentcases;
+package com.kayako.sdk.android.k5.messenger.homescreenpage.adapter.widget.recentcases;
 
 import com.kayako.sdk.android.k5.common.adapter.messengerlist.helper.DiffUtilsHelper;
 import com.kayako.sdk.android.k5.messenger.data.conversation.viewmodel.ClientTypingActivity;
@@ -11,15 +11,19 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 import static org.hamcrest.core.Is.is;
+
+import static  org.hamcrest.CoreMatchers.equalTo;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 
 public class RecentConversationsWidgetListItemTest {
     private static final String TITLE = "Test title";
@@ -28,6 +32,12 @@ public class RecentConversationsWidgetListItemTest {
     private OnClickRecentConversationListener onClickRecentConversationListener;
     private ConversationViewModel conversationViewModel;
     private BaseWidgetListItem.OnClickActionListener onClickActionListener;
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Rule
+    public final ErrorCollector collector = new ErrorCollector();
 
     @Before
     public void setUp() {
@@ -56,27 +66,21 @@ public class RecentConversationsWidgetListItemTest {
 
     @Test
     public void testIllegalArgumentoExceptionForConversations() {
-        try {
-            List<ConversationViewModel> emptyConversations = new ArrayList<>();
-            RecentConversationsWidgetListItem recentConversationsWidgetListItem =
-                    new RecentConversationsWidgetListItem(TITLE,
-                            "Action Button Label", onClickActionListener, emptyConversations,
-                            onClickRecentConversationListener);
-        } catch (IllegalArgumentException e) {
-            assertNotNull(e.getMessage());
-        }
+        thrown.expect(IllegalArgumentException.class);
+        List<ConversationViewModel> emptyConversations = new ArrayList<>();
+        RecentConversationsWidgetListItem recentConversationsWidgetListItem =
+                new RecentConversationsWidgetListItem(TITLE,
+                        "Action Button Label", onClickActionListener, emptyConversations,
+                        onClickRecentConversationListener);
     }
 
     @Test
     public void testIllegalArgumentoExceptionForOnClickRecentConversationListener() {
-        try {
-            RecentConversationsWidgetListItem recentConversationsWidgetListItem =
-                    new RecentConversationsWidgetListItem(TITLE,
-                            "Action Button Label", onClickActionListener, conversations,
-                            null);
-        } catch (IllegalArgumentException e) {
-            assertNotNull(e.getMessage());
-        }
+        thrown.expect(IllegalArgumentException.class);
+        RecentConversationsWidgetListItem recentConversationsWidgetListItem =
+                new RecentConversationsWidgetListItem(TITLE,
+                        "Action Button Label", onClickActionListener, conversations,
+                        null);
     }
 
     @Test
@@ -88,9 +92,11 @@ public class RecentConversationsWidgetListItemTest {
     @Test
     public void getContents() {
         Map<String, String> contents = recentConversationsWidgetListItem.getContents();
-        assertThat(contents.get("title"), is(TITLE));
-        assertThat(contents.get("conversations"), is(String.valueOf(conversations.size())));
-        assertThat(contents.get("conversations_" + conversationViewModel.getConversationId()),
-                is(DiffUtilsHelper.convertToString(conversationViewModel.getContents())));
+
+        collector.checkThat(contents.get("conversations"), equalTo(String.valueOf(
+                conversations.size())));
+        collector.checkThat(contents.get("title"), equalTo(TITLE));
+        collector.checkThat(contents.get("conversations_" + conversationViewModel.getConversationId()),
+                equalTo(DiffUtilsHelper.convertToString(conversationViewModel.getContents())));
     }
 }
