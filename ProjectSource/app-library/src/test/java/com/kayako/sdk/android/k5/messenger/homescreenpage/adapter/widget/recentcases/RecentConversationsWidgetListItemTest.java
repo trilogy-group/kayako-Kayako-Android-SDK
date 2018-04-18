ba774906ea674dc500b567ad5a477f900b1549cd
Pedro Henrique <pedro.henrique@aurea.com>
2018-04-18 18:08:03 -0300

    Fixing for requested changes

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/messenger/homescreenpage/adapter/widget/recentcases/RecentConversationsWidgetListItemTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/messenger/homescreenpage/adapter/widget/recentcases/RecentConversationsWidgetListItemTest.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
 public class RecentConversationsWidgetListItemTest {
     private static final String TITLE = "Test title";
     private List<ConversationViewModel> conversations;
