e0b65180201a4e84423d94e357acf0eb1a63ac2c
intipadillaaurea <38227218+intipadillaaurea@users.noreply.github.com>
2018-04-28 03:43:19 -0400

    increase coverage
    increase coverage

--- ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/utils/file/FileDownloadUtilTest.java
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/common/utils/file/FileDownloadUtilTest.java
@@ -36,6 +36,19 @@ public class FileDownloadUtilTest {
     private static final String URL_DOWNLOAD = "downloadurl";
     private static final String ATTACHMENT_NAME = "testAttach";
     private static final long SIZE = 10;
+    private static final String PAUSED_QUEUED_FOR_WIFI = "PAUSED_QUEUED_FOR_WIFI";
+    private static final String PAUSED_UNKNOWN = "PAUSED_UNKNOWN";
+    private static final String PAUSED_WAITING_FOR_NETWORK = "PAUSED_WAITING_FOR_NETWORK";
+    private static final String PAUSED_WAITING_TO_RETRY = "PAUSED_WAITING_TO_RETRY";
+    private static final String ERROR_CANNOT_RESUME = "ERROR_CANNOT_RESUME";
+    private static final String ERROR_DEVICE_NOT_FOUND = "ERROR_DEVICE_NOT_FOUND";
+    private static final String ERROR_FILE_ALREADY_EXISTS = "ERROR_FILE_ALREADY_EXISTS";
+    private static final String ERROR_FILE_ERROR = "ERROR_FILE_ERROR";
+    private static final String ERROR_HTTP_DATA_ERROR = "ERROR_HTTP_DATA_ERROR";
+    private static final String ERROR_INSUFFICIENT_SPACE = "ERROR_INSUFFICIENT_SPACE";
+    private static final String ERROR_TOO_MANY_REDIRECTS = "ERROR_TOO_MANY_REDIRECTS";
+    private static final String ERROR_UNHANDLED_HTTP_CODE = "ERROR_UNHANDLED_HTTP_CODE";
+    private static final String ERROR_UNKNOWN = "ERROR_UNKNOWN";
 
     @Mock
     private Context context;
@@ -50,10 +63,7 @@ public class FileDownloadUtilTest {
     @Mock
     private DownloadManager.Request request;
 
-    @Test
-    public void givenContextWhenCheckDownloadStatusThenReturnString()
-            throws Exception {
-        //Arrange
+    private void arrangeSwitchCases() throws Exception {
         when(context.getSystemService(Context.DOWNLOAD_SERVICE))
                 .thenReturn(downloadManager);
         whenNew(DownloadManager.Query.class).withNoArguments()
@@ -61,6 +71,13 @@ public class FileDownloadUtilTest {
         doReturn(null).when(query).setFilterById(DOWNLOAD_ID);
         when(downloadManager.query(query)).thenReturn(cursor);
         when(cursor.moveToFirst()).thenReturn(Boolean.TRUE);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
         when(cursor.getInt(COLUMN_INDEX)).thenReturn(STATUS);
         when(context.getString(R.string.ko__attachment_msg_download_successful))
                 .thenReturn(SUCCESSFUL_STATUS);
@@ -144,5 +161,365 @@ public class FileDownloadUtilTest {
 
         //Assert
         assertEquals(DOWNLOAD_ID, returnValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString2()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_RUNNING);
+        when(cursor.getInt(DownloadManager.STATUS_RUNNING))
+                .thenReturn(DownloadManager.STATUS_RUNNING);
+        when(context.getString(R.string.ko__attachment_msg_download_running))
+                .thenReturn(SUCCESSFUL_STATUS);
+
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString3()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_PENDING);
+        when(cursor.getInt(DownloadManager.STATUS_PENDING))
+                .thenReturn(DownloadManager.STATUS_PENDING);
+        when(context.getString(R.string.ko__attachment_msg_download_pending))
+                .thenReturn(SUCCESSFUL_STATUS);
+
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString4()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_RUNNING);
+        when(cursor.getInt(DownloadManager.STATUS_RUNNING))
+                .thenReturn(DownloadManager.STATUS_RUNNING);
+        when(context.getString(R.string.ko__attachment_msg_download_running))
+                .thenReturn(SUCCESSFUL_STATUS);
+
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString5()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(cursor.getInt(DownloadManager.STATUS_PAUSED))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(context.getString(R.string.ko__attachment_msg_download_paused))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.PAUSED_QUEUED_FOR_WIFI);
+        when(cursor.getInt(DownloadManager.PAUSED_QUEUED_FOR_WIFI))
+                .thenReturn(DownloadManager.PAUSED_QUEUED_FOR_WIFI);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+PAUSED_QUEUED_FOR_WIFI, returnedValue);
+    }
+
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString6()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(cursor.getInt(DownloadManager.STATUS_PAUSED))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(context.getString(R.string.ko__attachment_msg_download_paused))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.PAUSED_UNKNOWN);
+        when(cursor.getInt(DownloadManager.PAUSED_UNKNOWN))
+                .thenReturn(DownloadManager.PAUSED_UNKNOWN);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+PAUSED_UNKNOWN, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString7()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(cursor.getInt(DownloadManager.STATUS_PAUSED))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(context.getString(R.string.ko__attachment_msg_download_paused))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.PAUSED_WAITING_FOR_NETWORK);
+        when(cursor.getInt(DownloadManager.PAUSED_WAITING_FOR_NETWORK))
+                .thenReturn(DownloadManager.PAUSED_WAITING_FOR_NETWORK);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+PAUSED_WAITING_FOR_NETWORK, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString8()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(cursor.getInt(DownloadManager.STATUS_PAUSED))
+                .thenReturn(DownloadManager.STATUS_PAUSED);
+        when(context.getString(R.string.ko__attachment_msg_download_paused))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.PAUSED_WAITING_TO_RETRY);
+        when(cursor.getInt(DownloadManager.PAUSED_WAITING_TO_RETRY))
+                .thenReturn(DownloadManager.PAUSED_WAITING_TO_RETRY);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+PAUSED_WAITING_TO_RETRY, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString9()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_CANNOT_RESUME);
+        when(cursor.getInt(DownloadManager.ERROR_CANNOT_RESUME))
+                .thenReturn(DownloadManager.ERROR_CANNOT_RESUME);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_CANNOT_RESUME, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString10()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_DEVICE_NOT_FOUND);
+        when(cursor.getInt(DownloadManager.ERROR_DEVICE_NOT_FOUND))
+                .thenReturn(DownloadManager.ERROR_DEVICE_NOT_FOUND);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_DEVICE_NOT_FOUND, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString11()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_FILE_ALREADY_EXISTS);
+        when(cursor.getInt(DownloadManager.ERROR_FILE_ALREADY_EXISTS))
+                .thenReturn(DownloadManager.ERROR_FILE_ALREADY_EXISTS);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_FILE_ALREADY_EXISTS, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString12()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_FILE_ERROR);
+        when(cursor.getInt(DownloadManager.ERROR_FILE_ERROR))
+                .thenReturn(DownloadManager.ERROR_FILE_ERROR);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_FILE_ERROR, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString13()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_HTTP_DATA_ERROR);
+        when(cursor.getInt(DownloadManager.ERROR_HTTP_DATA_ERROR))
+                .thenReturn(DownloadManager.ERROR_HTTP_DATA_ERROR);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_HTTP_DATA_ERROR, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString14()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_INSUFFICIENT_SPACE);
+        when(cursor.getInt(DownloadManager.ERROR_INSUFFICIENT_SPACE))
+                .thenReturn(DownloadManager.ERROR_INSUFFICIENT_SPACE);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_INSUFFICIENT_SPACE, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString15()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_TOO_MANY_REDIRECTS);
+        when(cursor.getInt(DownloadManager.ERROR_TOO_MANY_REDIRECTS))
+                .thenReturn(DownloadManager.ERROR_TOO_MANY_REDIRECTS);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_TOO_MANY_REDIRECTS, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString16()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_UNHANDLED_HTTP_CODE);
+        when(cursor.getInt(DownloadManager.ERROR_UNHANDLED_HTTP_CODE))
+                .thenReturn(DownloadManager.ERROR_UNHANDLED_HTTP_CODE);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_UNHANDLED_HTTP_CODE, returnedValue);
+    }
+
+    @Test
+    public void givenContextWhenCheckDownloadStatusThenReturnString17()
+            throws Exception {
+        //Arrange
+        arrangeSwitchCases();
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(cursor.getInt(DownloadManager.STATUS_FAILED))
+                .thenReturn(DownloadManager.STATUS_FAILED);
+        when(context.getString(R.string.ko__attachment_msg_download_failed))
+                .thenReturn(SUCCESSFUL_STATUS);
+        when(cursor.getColumnIndex(DownloadManager.COLUMN_REASON))
+                .thenReturn(DownloadManager.ERROR_UNKNOWN);
+        when(cursor.getInt(DownloadManager.ERROR_UNKNOWN))
+                .thenReturn(DownloadManager.ERROR_UNKNOWN);
+        //Act
+        String returnedValue = FileDownloadUtil
+                .checkDownloadStatus(context, DOWNLOAD_ID);
+
+        //Assert
+        assertEquals(SUCCESSFUL_STATUS+ERROR_UNKNOWN, returnedValue);
     }
 }
