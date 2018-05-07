75a5fd8cb6a2033f590c866a8581b198a9e01728
Andres Rovira <andres.rovira@aurea.com>
2018-05-07 17:29:56 -0600

    Unit test for KreSubscription class

---
+++ ProjectSource/app-library/src/test/java/com/kayako/sdk/android/k5/kre/base/KreSubscriptionTest.java
@@ -0,0 +1,264 @@
+package com.kayako.sdk.android.k5.kre.base;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.validateMockitoUsage;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+
+import android.os.AsyncTask;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.kayako.sdk.android.k5.kre.base.KreSubscription.OnEventListener;
+import com.kayako.sdk.android.k5.kre.base.KreSubscription.OnSubscriptionListener;
+import com.kayako.sdk.android.k5.kre.base.credentials.KreCredentials;
+import com.kayako.sdk.android.k5.kre.data.PushData;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.phoenixframework.channels.Channel;
+import org.phoenixframework.channels.IMessageCallback;
+import org.phoenixframework.channels.Push;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+    KreSubscription.class,
+    KreConnection.class,
+    KreCredentials.class,
+    AsyncTask.class,
+    OnSubscriptionListener.class,
+    OnEventListener.class,
+    Channel.class,
+    Push.class
+})
+public class KreSubscriptionTest {
+
+  private KreSubscription kreSubscription;
+
+  private static final String SUBSCRIPTION_NAME = "testSubscription";
+  private static final String EVENT_NAME = "testEvent";
+  private static final String CHANNEL_NAME = "testChannel";
+  private static final String OTHER_CHANNEL_NAME = "testChannel2";
+  private static final String SUBSCRIBE_FAIL_NO_CLOSE = "Failed to start new subscription since previous subscription wasn't closed. Open page again to make it work!";
+  private static final String ERROR_MESSAGE_LISTEN_BEFORE_SUBSCRIBE = "call subscribe() before listening for an event";
+
+  @Mock
+  private KreConnection kreConnection;
+  @Mock
+  private KreCredentials kreCredentials;
+  @Mock
+  private Channel channel;
+  @Mock
+  private Push push;
+  @Rule
+  public final ExpectedException thrown = ExpectedException.none();
+  @Captor
+  private ArgumentCaptor<JsonNode> jsonNodeArgumentCaptor;
+  @Captor
+  private ArgumentCaptor<KreConnection.OnOpenConnectionListener> openConnectionListenerArgumentCaptor;
+
+  @Before
+  public void setUp() throws Exception {
+    //suppress(constructor(AsyncTask.class));
+    suppress(method(AsyncTask.class, "executeOnExecutor"));
+
+
+    kreSubscription = new KreSubscription(kreConnection, SUBSCRIPTION_NAME);
+  }
+
+  @After
+  public void validate() {
+    validateMockitoUsage();
+  }
+
+  @Test
+  public void shouldThrowExceptionOnConstructorIfConnectionIsNull() {
+    // Arrange
+
+    // Assert
+    thrown.expect(IllegalArgumentException.class);
+
+    // Act
+    kreSubscription = new KreSubscription(null, SUBSCRIPTION_NAME);
+  }
+
+  @Test
+  public void shouldSetDefaultNameIfNullNameOnConstructor() {
+    // Arrange
+
+    // Act
+    kreSubscription = new KreSubscription(kreConnection, null);
+
+    // Assert
+    assertThat(Whitebox.getInternalState(kreSubscription, "mTagWithName"),
+        is(equalTo(Whitebox.getInternalState(KreSubscription.class, "TAG"))));
+  }
+
+  @Test
+  public void shouldPrefixTagToNameOnConstructor() {
+    // Arrange
+    String expectedName = String.format("%s-%s",
+        Whitebox.getInternalState(KreSubscription.class, "TAG"),
+        SUBSCRIPTION_NAME);
+
+    // Act
+
+    // Assert
+    assertThat(Whitebox.getInternalState(kreSubscription, "mTagWithName").toString(),
+        is(equalTo(expectedName)));
+  }
+
+  @Test
+  public void subscribeToNewChannel() {
+    // Arrange
+    OnSubscriptionListener onSubscriptionListener = mock(OnSubscriptionListener.class);
+
+    // Act
+    kreSubscription.subscribe(kreCredentials, CHANNEL_NAME, onSubscriptionListener);
+
+    // Assert
+    verify(kreConnection).connect(eq(kreCredentials), eq(CHANNEL_NAME), openConnectionListenerArgumentCaptor.capture(), jsonNodeArgumentCaptor.capture());
+    assertNotNull(openConnectionListenerArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void listenForEventAfterSubscribe() throws Exception {
+    // Arrange
+    setUpChannel();
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    OnEventListener listener = mock(OnEventListener.class);
+    OnSubscriptionListener onSubscriptionListener = mock(OnSubscriptionListener.class);
+
+    // Act
+    kreSubscription.subscribe(kreCredentials, CHANNEL_NAME, onSubscriptionListener);
+    KreConnection.OnOpenConnectionListener connectionListener = Whitebox.getInternalState(kreSubscription, "mKreConnectionListener");
+    connectionListener.onOpen(channel);
+    kreSubscription.listenFor(EVENT_NAME, listener);
+
+    // Assert
+    verify(kreConnection).connect(eq(kreCredentials), eq(CHANNEL_NAME), openConnectionListenerArgumentCaptor.capture(), jsonNodeArgumentCaptor.capture());
+    assertNotNull(openConnectionListenerArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void triggerEventAfterSubscribe() throws Exception {
+    // Arrange
+    setUpChannel();
+    OnSubscriptionListener subscriptionListener = mock(OnSubscriptionListener.class);
+    OnEventListener eventListener = mock(OnEventListener.class);
+    PushData pushData = new PushData() {
+      @Override
+      public String toString() {
+        return "testPushData";
+      }
+
+      @Override
+      public boolean equals(Object o) {
+        return o.toString().equals(this.toString());
+      }
+
+      @Override
+      public int hashCode() {
+        return super.hashCode();
+      }
+    };
+
+    // Act
+    kreSubscription.subscribe(kreCredentials, CHANNEL_NAME, subscriptionListener);
+    KreConnection.OnOpenConnectionListener connectionListener = Whitebox.getInternalState(kreSubscription, "mKreConnectionListener");
+    connectionListener.onOpen(channel);
+    kreSubscription.listenFor(EVENT_NAME, eventListener);
+    boolean actual = kreSubscription.triggerEvent(EVENT_NAME, pushData);
+
+    // Assert
+    assertTrue(actual);
+  }
+
+  @Test
+  public void triggerEventBeforeSubscribe() {
+    // Arrange
+    PushData pushData = new PushData() {
+      @Override
+      public String toString() {
+        return "testPushData";
+      }
+
+      @Override
+      public boolean equals(Object o) {
+        return o.toString().equals(this.toString());
+      }
+
+      @Override
+      public int hashCode() {
+        return super.hashCode();
+      }
+    };
+
+    // Act
+    boolean actual = kreSubscription.triggerEvent(EVENT_NAME, pushData);
+
+    // Assert
+    assertFalse(actual);
+  }
+
+  @Test
+  public void subscribeAndThenUnsubscribe() throws Exception {
+    // Arrange
+    OnSubscriptionListener onSubscriptionListener = mock(OnSubscriptionListener.class);
+
+    // Act
+    kreSubscription.subscribe(kreCredentials, CHANNEL_NAME, onSubscriptionListener);
+    kreSubscription.unSubscribe(onSubscriptionListener);
+
+    // Assert
+    verify(kreConnection).connect(eq(kreCredentials), eq(CHANNEL_NAME), openConnectionListenerArgumentCaptor.capture(), jsonNodeArgumentCaptor.capture());
+    assertNotNull(openConnectionListenerArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void hasSubscribedShouldCallInternalObjectGet() throws Exception {
+    // Arrange
+    AtomicBoolean mHasSubscribedSuccessfully = Whitebox.getInternalState(kreSubscription, "mHasSubscribedSuccessfully");
+    boolean expected = mHasSubscribedSuccessfully.get();
+
+    // Act
+    boolean actual = kreSubscription.hasSubscribed();
+
+    // Verify
+    assertThat(actual, is(expected));
+  }
+
+  private void setUpChannel() throws Exception {
+    ArgumentCaptor<IMessageCallback> captor = ArgumentCaptor.forClass(IMessageCallback.class);
+    when(channel.join()).thenReturn(push);
+    when(push.receive(eq(Whitebox.getInternalState(KreSubscription.class, "EVENT_OK").toString()), captor.capture()))
+        .then(new Answer<Push>() {
+          @Override
+          public Push answer(InvocationOnMock invocationOnMock) throws Throwable {
+            AtomicBoolean mHasSubscribedSuccessfully = Whitebox.getInternalState(kreSubscription, "mHasSubscribedSuccessfully");
+            mHasSubscribedSuccessfully.set(true);
+            return push;
+          }
+        });
+  }
+
+}
